1cf6a1db130120cb6d44e7378e30c5b90a0ef7e6
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-01-08 17:45:26 +0100

    add truncateSentence func

--- leetcode/TruncateSentence.java
+++ leetcode/TruncateSentence.java
@@ -1,3 +1,14 @@
 public class TruncateSentence {
 
+    public String truncateSentence(String s, int k) {
+        if (s == null || s.length() == 0) return "";
+        String[] words = s.split(" ");
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < k; i++) {
+            str.append(words[i]).append(' ');
+        }
+
+        return str.toString().trim();
+
+    }
 }
