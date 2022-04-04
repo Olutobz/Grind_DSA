aa582dd84e0ec44363fedeb386f49065aba09a69
Onikoyi Damola Olutoba <Damexxey94@gmail.com>
2022-04-04 16:31:21 +0100

    add sum method

--- google/interviews/basics.java
+++ google/interviews/basics.java
@@ -4,9 +4,15 @@ public class basics {
     String firstName = "Damola";
     String middleName = "Olutoba";
     String lastName = "Onikoyi";
+    static int a = 24;
+    static int b = 49;
 
     void display(String fname, String mName,String lName) {
         System.out.println(fname + mName + lastName);
+    }
+    
+    int sum () {
+        return a + b;
     }
 
     public static void main(String[] args) {
