cda38bfb98776db78044b4e4f0611b0424eb55c7
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-04-28 15:42:18 +0100

    add SquaresOfASortedArray function

---
+++ leetcode/SquaresOfASortedArray.java
@@ -0,0 +1,26 @@
+public class SquaresOfASortedArray {
+    // TC -> O(N), SC -> O(1) (output array is ignored)
+    public int[] sortedSquares(int[] nums) {
+        if (nums == null || nums.length == 0) return null;
+
+        int[] newArr = new int[nums.length];
+        int start = 0;
+        int end = nums.length - 1;
+        int index = newArr.length - 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] *= nums[i];
+        }
+
+        while (start <= end) {
+            if (nums[start] > nums[end]) {
+                newArr[index] = nums[start++];
+            } else {
+                newArr[index] = nums[end--];
+            }
+            index--;
+        }
+        // return an array of the squares of each number sorted in non-decreasing order.
+        return newArr;
+    }
+}
