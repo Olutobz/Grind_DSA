71f424180361997146c019a2597fcb6af0ba959c
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-05-03 23:50:29 +0100

    add time & space complexity for searchInsert

--- leetcode/SearchInsertPosition.java
+++ leetcode/SearchInsertPosition.java
@@ -1,13 +1,13 @@
 public class SearchInsertPosition {
 
+    // TC -> O(LogN), SC -> O(1)
     public int searchInsert(int[] nums, int target) {
-        if (nums == null || nums.length == 0) return -1;
+        if (nums == null || nums.length == 0) return 0;
         int low = 0;
         int high = nums.length - 1;
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
-
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] > target) {
