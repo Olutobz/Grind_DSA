7bd3ebc7fae8eea92c68efe976d0665b9de92f35
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-05-03 23:47:46 +0100

    add HeightChecker

---
+++ leetcode/HeightChecker.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class HeightChecker {
+    public int heightChecker(int[] heights) {
+        int[] expected = new int[heights.length];
+        for (int i = 0; i < heights.length; i++) {
+            expected[i] = heights[i];
+        }
+        Arrays.sort(expected);
+        int count = 0;
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] != expected[i]) count++;
+        }
+
+        return count;
+    }
+}
