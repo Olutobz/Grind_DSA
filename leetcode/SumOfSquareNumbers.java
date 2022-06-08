95fc8da3b3cb967f6974062db30ce20bd47e1276
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-06-08 15:11:16 +0100

    add function to find the sum of square numbers

---
+++ leetcode/SumOfSquareNumbers.java
@@ -0,0 +1,12 @@
+public class SumOfSquareNumbers {
+    // TC -> O(C), SC -> O(1)
+    public boolean judgeSquareSum(int c) {
+        for (long a = 0; a * a <= c; a++) {
+            for (long b = 0; b * b <= c; b++) {
+                if (a * a + b * b == c)
+                    return true;
+            }
+        }
+        return false;
+    }
+}
