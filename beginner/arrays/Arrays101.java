e6811d17cf413e8ca0b33d5abc692b6151267ecf
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-07-15 19:23:46 +0100

    add method to merge two sorted arrays

--- beginner/arrays/Arrays101.java
+++ beginner/arrays/Arrays101.java
@@ -134,4 +134,27 @@ public class Arrays101 {
         return newArr;
     }
 
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int i = m+n-1; i>=0; i--) {
+            if(m>0 && n>0) {
+                if(nums2[n-1] > nums1[m-1]) {
+                    nums1[i] = nums2[n-1];
+                    n--;
+                }
+                else {
+                    nums1[i] = nums1[m-1];
+                    m--;
+                }
+            }
+            else if (m>0) {
+                nums1[i] = nums1[m-1];
+                m--;
+            }
+            else {
+                nums1[i] = nums2[n-1];
+                n--;
+            }
+        }
+    }
+
 }
