6a38d03a20a1021787060faa7d2cdfc0b1660b44
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-07-15 19:24:34 +0100

    reformat code

--- beginner/arrays/Arrays101.java
+++ beginner/arrays/Arrays101.java
@@ -135,23 +135,20 @@ public class Arrays101 {
     }
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for(int i = m+n-1; i>=0; i--) {
-            if(m>0 && n>0) {
-                if(nums2[n-1] > nums1[m-1]) {
-                    nums1[i] = nums2[n-1];
+        for (int i = m + n - 1; i >= 0; i--) {
+            if (m > 0 && n > 0) {
+                if (nums2[n - 1] > nums1[m - 1]) {
+                    nums1[i] = nums2[n - 1];
                     n--;
-                }
-                else {
-                    nums1[i] = nums1[m-1];
+                } else {
+                    nums1[i] = nums1[m - 1];
                     m--;
                 }
-            }
-            else if (m>0) {
-                nums1[i] = nums1[m-1];
+            } else if (m > 0) {
+                nums1[i] = nums1[m - 1];
                 m--;
-            }
-            else {
-                nums1[i] = nums2[n-1];
+            } else {
+                nums1[i] = nums2[n - 1];
                 n--;
             }
         }
