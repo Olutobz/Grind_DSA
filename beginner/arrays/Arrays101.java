369ba1861dd15b78fc12c0ef27802df1eacc879c
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-07-17 06:15:30 +0100

    add a function to remove element from an array

--- beginner/arrays/Arrays101.java
+++ beginner/arrays/Arrays101.java
@@ -152,4 +152,15 @@ public class Arrays101 {
         }
     }
 
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        if(nums.length == 0) return 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != val) {
+                nums[k++] = nums[i];
+            }
+        }
+        return k;
+    }
+
 }
