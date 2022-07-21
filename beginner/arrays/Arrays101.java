32e30564fbb2fb72a611afe0d01dd3c018c7d329
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-07-21 23:58:06 +0100

    add other implementation of mountainArray problem

--- beginner/arrays/Arrays101.java
+++ beginner/arrays/Arrays101.java
@@ -21,7 +21,7 @@ public class Arrays101 {
         return max;
     }
 
-    private int countDigits(Long digits) {
+    public int countDigits(Long digits) {
         int count = 0;
         while (digits != 0) {
             digits = digits / 10;
@@ -197,20 +197,52 @@ public class Arrays101 {
     }
 
     public boolean validMountainArray(int[] arr) {
-        int start = 0;
-        int end = arr.length - 1;
-        while(start != end) {
-            if(arr[start] < arr[start + 1]) {
+        int start = 0, end = arr.length - 1;
+        while (start != end) {
+            if (arr[start] < arr[start + 1]) {
                 start++;
-            }
-            else if(arr[end] < arr[end - 1]) {
+            } else if (arr[end] < arr[end - 1]) {
                 end--;
-            }
-            else {
+            } else {
                 return false;
             }
         }
         return start != 0 && end != arr.length - 1;
+    }
+
+    public boolean validMountainArray2(int[] arr) {
+        if (arr.length < 3) return false;
+        int n = arr.length, start = 0, end = n - 1;
+
+        // walk up
+        while (start + 1 < n && arr[start] < arr[start + 1]) {
+            start++;
+        }
+        // peak cannot be first or last
+        if (start == 0 || start == end) {
+            return false;
+        }
+        // walk down
+        while (start + 1 < n && arr[start] > arr[start + 1]) {
+            start++;
+        }
+        // walked back to the ground
+        return start == end;
+    }
+
+    public boolean validMountainArray3(int[] arr) {
+        if (arr.length < 3) return false;
+        int n = arr.length, i = 0, j = n - 1;
+
+        // walk up
+        while (i + 1 < n && arr[i] < arr[i + 1]) {
+            i++;
+        }
+        // walk down
+        while (j - 1 < n && arr[j] < arr[j - 1]) {
+            j--;
+        }
+        return i > 0 && j < n - 1 && i == j;
     }
 
 }
