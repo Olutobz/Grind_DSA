4a1b3ff9264c9915c6bb4ed410d76eef5389f292
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-07-23 10:18:37 +0100

    add function to move zeros to end of the array

--- beginner/arrays/Arrays101.java
+++ beginner/arrays/Arrays101.java
@@ -272,5 +272,17 @@ public class Arrays101 {
         return arr;
     }
 
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index++] = nums[i];
+            }
+        }
+        while (index < nums.length) {
+            nums[index++] = 0;
+        }
+    }
+
 }
 
