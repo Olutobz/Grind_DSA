4663f7b9c9cf61125ef5565e3a1376847b67112a
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-07-27 23:19:26 +0100

    add findDisappearedNumbers function

--- easy/arrays/Arrays101.java
+++ easy/arrays/Arrays101.java
@@ -360,5 +360,22 @@ public class Arrays101 {
         else return max1;
     }
 
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) list.add(i + 1);
+        }
+
+        return list;
+    }
+
 }
 
