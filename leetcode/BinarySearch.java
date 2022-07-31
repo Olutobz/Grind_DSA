e47d8b0b211f2dd6f73b384b8424172d775a517b
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-07-31 22:34:49 +0100

    add BinarySearch function

---
+++ leetcode/BinarySearch.java
@@ -0,0 +1,17 @@
+public class BinarySearch {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return -1;
+        int start = 0, end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
