0cff0f191a900ff8be14d4fdc921c7fb3e82f210
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-08-05 02:25:57 +0100

    add maximumProduct function solution

--- leetcode/arraysNstrings/ArraysAndStrings.java
+++ leetcode/arraysNstrings/ArraysAndStrings.java
@@ -1,5 +1,7 @@
 package arraysNstrings;
 
+import java.util.Arrays;
+
 public class ArraysAndStrings {
 
     // TC -> O(N) SC -> O(N)
@@ -41,7 +43,6 @@ public class ArraysAndStrings {
 
     public int dominantIndex(int[] nums) {
         if (nums == null || nums.length == 0) return -1;
-
         int maxElem = Integer.MIN_VALUE, maxIndex = -1, count = 0;
 
         for (int i = 0; i < nums.length; i++) {
@@ -50,11 +51,9 @@ public class ArraysAndStrings {
                 maxIndex = i;
             }
         }
-
         for (int num : nums) {
             if (maxElem >= num * 2) count++;
         }
-
         if (count == nums.length - 1) return maxIndex;
 
         return -1;
@@ -62,9 +61,7 @@ public class ArraysAndStrings {
 
     public int dominantIndexII(int[] nums) {
         if (nums == null || nums.length == 0) return -1;
-
         int max1 = 0, max2 = 0, maxIndex = -1;
-
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] > max1) {
                 max2 = max1;
@@ -78,5 +75,37 @@ public class ArraysAndStrings {
         return (max1 >= max2 * 2) ? maxIndex : -1;
     }
 
+    @SuppressWarnings("DuplicatedCode")
+    private int maximumProduct(int[] nums) {
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num < min1) {
+                min2 = min1;
+                min1 = num;
+            } else if (num < min2) {
+                min2 = num;
+            }
+
+            if (num > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2) {
+                max3 = max2;
+                max2 = num;
+            } else if (num > max3) {
+                max3 = num;
+            }
+        }
+        return Math.max(max1 * max2 * max3, min1 * min2 * max1);
+    }
+
+    public int maximumProductII(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        return Math.max(nums[0] * nums[1] * nums[n - 1], nums[n - 1] * nums[n - 2] * nums[n - 3]);
+    }
 
 }
