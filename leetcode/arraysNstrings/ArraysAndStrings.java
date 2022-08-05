4eeffcea1224d3a08dab622d3b8e6a25bd96c425
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-08-05 19:32:22 +0100

    add plusOne method impl

--- leetcode/arraysNstrings/ArraysAndStrings.java
+++ leetcode/arraysNstrings/ArraysAndStrings.java
@@ -149,4 +149,18 @@ public class ArraysAndStrings {
         return left;
     }
 
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] newDigits = new int[n + 1];
+        newDigits[0] = 1;
+        return newDigits;
+    }
+
 }
