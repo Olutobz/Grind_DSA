2060752b63466e15db9cab9915c92d4c3e40019e
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-08-05 04:25:21 +0100

    add solution to productExceptSelf and inplace operation alternative

--- leetcode/arraysNstrings/ArraysAndStrings.java
+++ leetcode/arraysNstrings/ArraysAndStrings.java
@@ -108,4 +108,45 @@ public class ArraysAndStrings {
         return Math.max(nums[0] * nums[1] * nums[n - 1], nums[n - 1] * nums[n - 2] * nums[n - 3]);
     }
 
+    public int[] productExceptSelf(int[] nums) {
+        int length = nums.length;
+        int[] answer = new int[length];
+        int[] left = new int[length];
+        int[] right = new int[length];
+
+        left[0] = 1;
+        for (int i = 1; i < length; i++) {
+            left[i] = left[i - 1] * nums[i - 1];
+        }
+
+        right[length - 1] = 1;
+        for (int i = length - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * nums[i + 1];
+        }
+
+        for (int k = 0; k < length; k++) {
+            answer[k] = left[k] * right[k];
+        }
+
+        return answer;
+
+    }
+
+    public int[] productExceptSelfII(int[] nums) {
+        int length = nums.length;
+        int[] left = new int[length];
+
+        for (int i = 0, temp = 1; i < length; i++) {
+            left[i] = temp;
+            temp *= nums[i];
+        }
+
+        for (int i = length - 1, temp = 1; i >= 0; i--) {
+            left[i] *= temp;
+            temp *= nums[i];
+        }
+
+        return left;
+    }
+
 }
