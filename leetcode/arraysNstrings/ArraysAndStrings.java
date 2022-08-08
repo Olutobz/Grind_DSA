116187853260d2998f394881516924b1aca8fcfe
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-08-08 07:49:24 +0100

    add reverseString function

--- leetcode/arraysNstrings/ArraysAndStrings.java
+++ leetcode/arraysNstrings/ArraysAndStrings.java
@@ -163,4 +163,15 @@ public class ArraysAndStrings {
         return newDigits;
     }
 
+    public void reverseString(char[] s) {
+        int first = 0, last = s.length - 1;
+        while (first < last) {
+            char temp = s[first];
+            s[first] = s[last];
+            s[last] = temp;
+            first++;
+            last--;
+        }
+    }
+
 }
