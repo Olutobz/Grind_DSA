6cc0b5e652092f265d7abfac22128eaf68cc2f41
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-08-09 09:01:00 +0100

    add method to find the sqrt of a non-negative integer

--- leetcode/arraysNstrings/ArraysAndStrings.java
+++ leetcode/arraysNstrings/ArraysAndStrings.java
@@ -221,4 +221,30 @@ public class ArraysAndStrings {
         return false;
     }
 
+    // TC -> O(sqrt(x)), SC -> O(1)
+    public int mySqrt(int x) {
+        if (x == 0) return 0;
+        for (int i = 1; i <= x / i; i++) {
+            if (i <= x / i && (i + 1) > x / (i + 1))
+                return i;
+        }
+        return -1;
+    }
+
+    // TC -> O(logX), SC -> O(1)
+    public int mySqrtII(int x) {
+        if (x == 0) return 0;
+        int left = 1;
+        int right = x;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid <= x / mid && (mid + 1) > x / (mid + 1)) return mid;
+            else if (mid > x / mid) right = mid - 1;
+            else left = mid + 1;
+        }
+
+        return left;
+    }
+
 }
