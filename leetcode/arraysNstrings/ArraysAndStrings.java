4d44a55795da5e190cd62da6978d06352dac5a25
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-08-10 18:59:14 +0100

    add method to find a perfect square

--- leetcode/arraysNstrings/ArraysAndStrings.java
+++ leetcode/arraysNstrings/ArraysAndStrings.java
@@ -237,4 +237,18 @@ public class ArraysAndStrings {
         return left;
     }
 
+    public boolean isPerfectSquare(int num) {
+        long left = 1, right = num;
+
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            if (mid * mid == num) return true;
+            else if (mid * mid < num) left = mid + 1;
+            else right = mid - 1;
+        }
+
+        return false;
+
+    }
+
 }
