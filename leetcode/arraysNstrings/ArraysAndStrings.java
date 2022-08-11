4a26cad253b90e3dee43c201032bb97f9e0c9120
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-08-11 15:41:17 +0100

    add alternative implementation of perfectSquare method

--- leetcode/arraysNstrings/ArraysAndStrings.java
+++ leetcode/arraysNstrings/ArraysAndStrings.java
@@ -248,4 +248,16 @@ public class ArraysAndStrings {
         return false;
     }
 
+    public boolean isPerfectSquareII(int num) {
+        if (num == 0 || num == 1) return true;
+        int start = 1, end = num;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (mid == num / mid && num % mid == 0) return true;
+            else if (mid < num / mid) start = mid + 1;
+            else end = mid - 1;
+        }
+        return false;
+    }
+
 }
