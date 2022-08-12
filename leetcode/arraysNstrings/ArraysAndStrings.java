07cb7b4610e7ad7add9ed67c22922b1c3d6d161b
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-08-12 16:49:55 +0100

    add new impl for sum of squares method

--- leetcode/arraysNstrings/ArraysAndStrings.java
+++ leetcode/arraysNstrings/ArraysAndStrings.java
@@ -1,6 +1,8 @@
 package arraysNstrings;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ArraysAndStrings {
 
@@ -288,7 +290,7 @@ public class ArraysAndStrings {
         return false;
     }
 
-    // TC -> O(N), SC -> O(1)
+    // TC -> O(sqrt(clog c)), SC -> O(1)
     public boolean judgeSquareSumIII(int c) {
         if (c < 3) return true;
         long a = 0, b = (int) Math.sqrt(c);
@@ -298,6 +300,17 @@ public class ArraysAndStrings {
             if (sum == c) return true;
             else if (sum < c) a++;
             else b--;
+        }
+        return false;
+    }
+
+    // TC -> O(sqrt c), SC -> O(N)
+    public boolean judgeSquareSumIV(int c) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int a = 0; a <= Math.sqrt(c); a++) {
+            set.add(a * a);
+            if (set.contains(c - a * a)) return true;
         }
         return false;
     }
