2759ca3ae64b816119c271538e31ed2713e5d0b4
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-08-14 05:26:30 +0100

    add impl for findDiagonalOrder function

--- leetcode/arraysNstrings/ArraysAndStrings.java
+++ leetcode/arraysNstrings/ArraysAndStrings.java
@@ -315,4 +315,39 @@ public class ArraysAndStrings {
         return false;
     }
 
+    public int[] findDiagonalOrder(int[][] mat) {
+        if (mat == null || mat.length == 0 || mat[0].length == 0) {
+            return new int[0];
+        }
+
+        int m = mat.length, n = mat[0].length;
+        int row = 0, col = 0;
+        int[] result = new int[m * n];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = mat[row][col];
+            // even sum diagonal -> go up
+            if ((row + col) % 2 == 0) {
+                if (col == n - 1) row++;
+                else if (row == 0) col++;
+                else {
+                    row--;
+                    col++;
+                }
+            }
+            // odd sum diagonal -> go down
+            else if ((row + col) % 2 == 1) {
+                if (row == m - 1) col++;
+                else if (col == 0) row++;
+                else {
+                    row++;
+                    col--;
+                }
+            }
+
+        }
+
+        return result;
+    }
+
 }
