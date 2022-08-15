57122f7b59743e788205863353e6a158c40cb6a2
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-08-15 16:30:18 +0100

    add function addDigits

--- leetcode/arrays/Arrays101.java
+++ leetcode/arrays/Arrays101.java
@@ -394,4 +394,19 @@ public class Arrays101 {
         return missing;
     }
 
+    // TC -> O(1), SC -> O(1)
+    public int addDigits(int num) {
+        if (num == 0) return 0;
+        while (num > 9) {
+            num = num / 10 + num % 10;
+        }
+        return num;
+    }
+
+    // TC -> O(1), SC -> O(1)
+    public int addDigitsII(int num) {
+        if (num == 0) return 0;
+        return num % 9 == 0 ? 9 : num % 9;
+    }
+
 }
