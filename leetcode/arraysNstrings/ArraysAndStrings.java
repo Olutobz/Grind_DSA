4f37829fea9c47e2e8e3fc32e565ff8d2d032947
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-08-16 04:02:36 +0100

    add hashing implementation of diagonalOrder function

--- leetcode/arraysNstrings/ArraysAndStrings.java
+++ leetcode/arraysNstrings/ArraysAndStrings.java
@@ -1,8 +1,6 @@
 package arraysNstrings;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class ArraysAndStrings {
 
@@ -315,6 +313,8 @@ public class ArraysAndStrings {
         return false;
     }
 
+    // TC -> O(M * N), M = number of rows, N = number of columns.
+    // SC -> O(1), (if we ignore the returned array)
     public int[] findDiagonalOrder(int[][] mat) {
         if (mat == null || mat.length == 0 || mat[0].length == 0) {
             return new int[0];
@@ -345,6 +345,37 @@ public class ArraysAndStrings {
                 }
             }
 
+        }
+
+        return result;
+    }
+
+    // TC -> O(M * N), SC -> O(N)
+    public int[] findDiagonalOrderII(int[][] mat) {
+        if (mat == null || mat.length == 0 || mat[0].length == 0) {
+            return new int[0];
+        }
+
+        Map<Integer, List<Integer>> diagonalMap = new HashMap<>();
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                int indexSum = i + j;
+                if (!diagonalMap.containsKey(indexSum)) {
+                    diagonalMap.put(indexSum, new ArrayList<>());
+                }
+
+                if (indexSum % 2 == 0) diagonalMap.get(indexSum).add(0, mat[i][j]);
+                else diagonalMap.get(indexSum).add(mat[i][j]);
+            }
+        }
+
+        int index = 0;
+        int[] result = new int[mat.length * mat[0].length];
+        for (int indexSum : diagonalMap.keySet()) {
+            for (int val : diagonalMap.get(indexSum)) {
+                result[index++] = val;
+            }
         }
 
         return result;
