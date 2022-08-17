e3566e6704060f0de1ecf2f12119f0c1f55aae5b
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-08-18 00:34:24 +0100

    Add class FindMaxConsecutiveOnes

---
+++ leetcode/FindMaxConsecutiveOnes.java
@@ -0,0 +1,15 @@
+public class FindMaxConsecutiveOnes {
+    // Given a binary array nums, return the maximum number of consecutive 1's in the array.
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0, max = 0;
+        for (int num : nums) {
+            if (num == 0) {
+                count = 0;
+            } else if (num == 1) {
+                count++;
+            }
+            max = Math.max(count, max);
+        }
+        return max;
+    }
+}
