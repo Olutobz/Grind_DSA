ebb487e7eaf82363489bd434157d6fefb1e1b4a3
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-08-19 11:27:37 +0100

    add function to find the longest consecutive sequence

---
+++ leetcode/LongestConsecutiveSequence.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestConsecutiveSequence {
+
+    public int longestConsecutive(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) return 1;
+        Set<Integer> set = new HashSet<>();
+        int longestStreak = 0;
+
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        for (int num : set) {
+            if (!set.contains(num - 1)) {
+                int currStreak = 1;
+
+                while (set.contains(num + 1)) {
+                    num++;
+                    currStreak++;
+                }
+                longestStreak = Math.max(currStreak, longestStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+
+}
