a5af787c787bdf3e513ee835e0832e83f5b62942
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-08-19 03:13:44 +0100

    modified and remove redundant code

--- leetcode/PascalTriangle.java
+++ leetcode/PascalTriangle.java
@@ -8,7 +8,7 @@ public class PascalTriangle {
         List<List<Integer>> pascal = new ArrayList<>();
 
         for (int i = 0; i < numRows; i++) {
-            List<Integer> list = new ArrayList<Integer>();
+            List<Integer> list = new ArrayList<>();
             for (int j = 0; j <= i; j++) {
                 if (j == 0 || j == i) list.add(1);
                 else {
