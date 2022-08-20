83ded2b44e6410c5fd3b717d0fb4ab41e47cdfe2
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-08-20 15:28:34 +0100

    modified code

--- leetcode/arraysNstrings/ArraysAndStrings.java
+++ leetcode/arraysNstrings/ArraysAndStrings.java
@@ -103,7 +103,7 @@ public class ArraysAndStrings {
     public int maximumProductII(int[] nums) {
         Arrays.sort(nums);
         int n = nums.length;
-        return Math.max(nums[0] * nums[1] * nums[n - 1], nums[n - 1] * nums[n - 2] * nums[n - 3]);
+        return Math.max(nums[0] * nums[1] * nums[n - 1], nums[n - 1] * nums[n - 2] * nums[n  - 3]);
     }
 
     // TC -> O(N), SC -> O(N)
