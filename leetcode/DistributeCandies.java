d23a00551d1f79a35a29f81f3e9f9df259fb0cc2
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-08-20 14:25:32 +0100

    add impl for DistributeCandies

---
+++ leetcode/DistributeCandies.java
@@ -0,0 +1,34 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class DistributeCandies {
+
+    public int distributeCandies(int[] candyType) {
+        int uniqueCandies = 0;
+
+        for (int i = 0; i < candyType.length; i++) {
+            boolean isUnique = true;
+            for (int j = 0; j < i; j++) {
+                if (candyType[i] == candyType[j]) {
+                    isUnique = false;
+                    break;
+                }
+            }
+            if (isUnique) uniqueCandies++;
+        }
+
+        return Math.min(uniqueCandies, candyType.length / 2);
+
+    }
+
+
+    public int distributeCandiesII(int[] candyType) {
+        Set<Integer> candiesSet = new HashSet<>();
+        int totalCandies = candyType.length;
+
+        for (int candy : candyType) candiesSet.add(candy);
+        return Math.min(candiesSet.size(), totalCandies / 2);
+
+    }
+
+}
