7fbf1e041094d934d590c3fd8172cdbac106fe80
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-08-21 09:03:04 +0100

    add time & space complexities for DistributeCandies

--- leetcode/DistributeCandies.java
+++ leetcode/DistributeCandies.java
@@ -3,9 +3,9 @@ import java.util.Set;
 
 public class DistributeCandies {
 
+    // TC -> O(N^2), SC -> O(1)
     public int distributeCandies(int[] candyType) {
         int uniqueCandies = 0;
-
         for (int i = 0; i < candyType.length; i++) {
             boolean isUnique = true;
             for (int j = 0; j < i; j++) {
@@ -21,12 +21,14 @@ public class DistributeCandies {
 
     }
 
-
+    // TC -> O(N), SC -> O(N)
     public int distributeCandiesII(int[] candyType) {
         Set<Integer> candiesSet = new HashSet<>();
         int totalCandies = candyType.length;
 
-        for (int candy : candyType) candiesSet.add(candy);
+        for (int candy : candyType) {
+            candiesSet.add(candy);
+        }
         return Math.min(candiesSet.size(), totalCandies / 2);
 
     }
