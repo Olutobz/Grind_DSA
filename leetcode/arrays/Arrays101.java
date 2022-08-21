a4bc43f9cd5dc2137f378f119266a0f3a1179b2d
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-08-21 13:26:10 +0100

    add function maximumWealth

--- leetcode/arrays/Arrays101.java
+++ leetcode/arrays/Arrays101.java
@@ -445,4 +445,28 @@ public class Arrays101 {
         return res;
     }
 
+    // TC -> O(M * N), SC -> O(1)
+    public int maximumWealth(int[][] accounts) {
+        if (accounts == null || accounts.length == 0) return 0;
+        int maxWealth = 0;
+
+        for (int[] customer : accounts) {
+            int sum = 0;
+            for (int bankMoney : customer) {
+                sum += bankMoney;
+            }
+            maxWealth = Math.max(maxWealth, sum);
+        }
+
+        /* for(int i=0; i<m; i++) {
+            int sum = 0;
+            for(int j=0; j<n; j++) {
+                sum += accounts[i][j];
+            }
+            maxWealth = Math.max(maxWealth, sum);
+        }
+        * */
+        return maxWealth;
+    }
+
 }
