b62610209f165c1ab96f35c38274cb95b2b72699
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-08-24 13:19:58 +0100

    add Two Sum

---
+++ leetcode/TwoSum.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    // TC -> O(N^2), SC -> O(1)
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return new int[0];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return new int[0];
+
+    }
+
+    // TC -> O(N), SC -> O(N)
+    public int[] twoSumII(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return new int[0];
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int potentialMatch = target - nums[i];
+            if (map.containsKey(potentialMatch)) {
+                return new int[]{map.get(potentialMatch), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+
+    }
+
+}
