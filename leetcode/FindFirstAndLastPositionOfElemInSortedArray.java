b80e3d8ae5d7999edcccfff80a0a0d66d892e354
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-08-30 11:38:56 +0100

    add FindFirstAndLastPositionOfElementInSortedArray

---
+++ leetcode/FindFirstAndLastPositionOfElemInSortedArray.java
@@ -0,0 +1,38 @@
+public class FindFirstAndLastPositionOfElemInSortedArray {
+    // TC -> O(LogN), SC -> O(N)
+    public int[] searchRange(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return new int[]{-1, -1};
+        int[] result = new int[2];
+        result[0] = findFirst(nums, target);
+        result[1] = findLast(nums, target);
+        return result;
+    }
+
+    private int findFirst(int[] arr, int target) {
+        int start = 0, end = arr.length - 1, index = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] >= target) {
+                end = mid - 1;
+            } else  {
+                start = mid + 1;
+            }
+            if (arr[mid] == target) index = mid;
+        }
+        return index;
+    }
+
+    private int findLast(int[] arr, int target) {
+        int start = 0, end = arr.length - 1, index = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] <= target) {
+                start = mid + 1;
+            } else  {
+                end = mid - 1;
+            }
+            if (arr[mid] == target) index = mid;
+        }
+        return index;
+    }
+}
