f5f8b2bc08137b418f0ef737a9d239bfae69f7b0
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-09-02 10:19:00 +0100

    add RemoveElement

---
+++ leetcode/RemoveElement.java
@@ -0,0 +1,30 @@
+public class RemoveElement {
+
+    // TC -> O(N), SC -> O(1)
+    public int removeElement(int[] nums, int val) {
+        if (nums.length == 0) return 0;
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k++] = nums[i];
+            }
+        }
+        return k;
+    }
+
+    // TC -> O(N), SC -> O(1)
+    public int removeElementII(int[] nums, int val) {
+        if (nums.length == 0) return 0;
+        int length = nums.length;
+        int i = 0;
+        while (i < length) {
+            if (nums[i] == val) {
+                nums[i] = nums[length - 1];
+                length--;
+            } else {
+                i++;
+            }
+        }
+        return length;
+    }
+}
