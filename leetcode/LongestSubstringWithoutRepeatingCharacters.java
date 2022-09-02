63909ee3847f12c1b96d534fd1931a15e9d7609f
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-09-02 11:56:06 +0100

    add LongestSubstringWithoutRepeatingCharacters

---
+++ leetcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+
+    // TC -> O(N), SC -> O(N)
+    public int lengthOfLongestSubstring(String s) {
+        int i = 0, j = 0, max = 0;
+        Set<Character> set = new HashSet<>();
+
+        while (j < s.length()) {
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                max = Math.max(max, set.size());
+            } else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return max;
+    }
+}
