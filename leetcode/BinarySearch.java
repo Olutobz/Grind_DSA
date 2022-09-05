cadc038076aeccbaf8fa4ad319945f60077d88de
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-09-05 22:42:32 +0100

    add time & space complexities for binary search

--- leetcode/BinarySearch.java
+++ leetcode/BinarySearch.java
@@ -1,4 +1,6 @@
 public class BinarySearch {
+
+    // TC -> O(LogN), SC -> O(1)
     public int search(int[] nums, int target) {
         if (nums == null || nums.length == 0) return -1;
         int start = 0, end = nums.length - 1;
