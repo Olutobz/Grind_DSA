824428b9df8737ee16b48c69e067754a3ebab9f5
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-09-06 16:22:38 +0100

    add another impl for LengthOfLongestSubstring

--- leetcode/LongestSubstringWithoutRepeatingCharacters.java
+++ leetcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,21 +1,40 @@
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class LongestSubstringWithoutRepeatingCharacters {
 
     // TC -> O(N), SC -> O(N)
     public int lengthOfLongestSubstring(String s) {
-        int i = 0, j = 0, max = 0;
+        int left = 0, right = 0, max = 0;
         Set<Character> set = new HashSet<>();
 
-        while (j < s.length()) {
-            if (!set.contains(s.charAt(j))) {
-                set.add(s.charAt(j++));
+        while (right < s.length()) {
+            if (!set.contains(s.charAt(right))) {
+                set.add(s.charAt(right++));
                 max = Math.max(max, set.size());
             } else {
-                set.remove(s.charAt(i++));
+                set.remove(s.charAt(left++));
             }
         }
         return max;
     }
+
+    // TC -> O(N), SC -> O(N)
+    public int lengthOfLongestSubstringII(String s) {
+        if (s.length() == 0) return 0;
+        Map<Character, Integer> map = new HashMap<>();
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0, j = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                j = Math.max(j, map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i), i);
+            max = Math.max(max, i - j + 1);
+        }
+        return max;
+    }
+
 }
