2298391de271c5c18f4a8ac71ab14f906bc49fea
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-09-09 04:08:46 +0100

    modified TransposeMatrix

--- leetcode/TransposeMatrix.java
+++ leetcode/TransposeMatrix.java
@@ -16,20 +16,17 @@ public class TransposeMatrix {
         return transposeMatrix;
     }
 
-    // TC -> O(R * C), SC -> O(R * C)
-    public int[][] transposeII(int[][] matrix) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
-            return new int[0][0];
-        }
-        int row = matrix.length, col = matrix[0].length;
-        int[][] transposeMatrix = new int[col][row];
-
-        for (int j = 0; j < col; j++) {
-            for (int i = 0; i < row; i++) {
-                transposeMatrix[j][i] = matrix[i][j];
+    // TC -> O(R * C) -> O(m ^ 2), SC -> O(1)
+    public int[][] transposeSquareMatrix(int[][] matrix) {
+        // if the size of row == size of col (A Square Matrix)
+        int m = matrix.length;
+        for (int i = 0; i < m; i++) {
+            for (int j = i + 1; j < m; j++) {
+                int temp = matrix[i][j];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = temp;
             }
         }
-        return transposeMatrix;
+        return matrix;
     }
-
 }
