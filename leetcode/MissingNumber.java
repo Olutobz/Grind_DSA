23bba4f9422f034a744b05768a44090a2a4e0b46
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-09-09 13:39:58 +0100

    add xor implementation for function MissingNumber

--- leetcode/MissingNumber.java
+++ leetcode/MissingNumber.java
@@ -13,4 +13,25 @@ public class MissingNumber {
         }
         return totalSum - sum;
     }
+
+    // TC -> O(N), SC -> O(1)
+    public int missingNumberII(int[] nums) {
+        if (nums == null || nums.length == 0) return -1;
+        int n = nums.length, sum = 0;
+        int total = n * (n + 1) / 2;
+        for (int num : nums) {
+            sum += num;
+        }
+        return total - sum;
+    }
+
+    // TC -> O(N), SC -> O(1)
+    public int missingNumberIII(int[] nums) {
+        if (nums == null || nums.length == 0) return -1;
+        int xor = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            xor = xor ^ i ^ nums[i]; // a^b^b = a
+        }
+        return xor;
+    }
 }
