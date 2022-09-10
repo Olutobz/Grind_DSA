7a01adb9a23432035ce91d4a80c793c2465972df
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-09-10 07:04:32 +0100

    add more impl for ValidAnagram

--- leetcode/ValidAnagram.java
+++ leetcode/ValidAnagram.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ValidAnagram {
 
@@ -12,5 +14,48 @@ public class ValidAnagram {
         Arrays.sort(s1);
         Arrays.sort(s2);
         return Arrays.equals(s1, s2);
+    }
+
+    // TC -> O(N), SC -> O(N)
+    public boolean isAnagramII(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) - 1);
+        }
+
+        for (char c : map.keySet()) {
+            if (map.get(c) != 0) return false;
+        }
+
+        return true;
+    }
+
+    // TC -> O(N), SC -> O(N)
+    public boolean isAnagramIII(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] freq = new int[26];
+
+        for (int c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        for (char c : t.toCharArray()) {
+            freq[c - 'a']--;
+        }
+
+        for (int count : freq) {
+            if (count != 0) return false;
+        }
+
+        return true;
     }
 }
