55e7388f7382f3bc8aa99a686752671cd81c98b7
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-09-14 00:04:14 +0100

    Add hashtable impl and Boyer–Moore majority vote algorithm

--- leetcode/MajorityElement.java
+++ leetcode/MajorityElement.java
@@ -1,9 +1,12 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MajorityElement {
 
     // TC -> O(N^2), SC -> O(1)
     public int majorityElement(int[] nums) {
+        if (nums.length == 1) return nums[0];
         int majorElem = nums.length / 2;
 
         for (int num : nums) {
@@ -24,9 +27,46 @@ public class MajorityElement {
 
     // TC -> O(NLogN), SC -> O(1)
     public int majorityElementI(int[] nums) {
+        if (nums.length == 1) return nums[0];
         int n = nums.length;
         Arrays.sort(nums);
         return nums[n / 2];
+    }
+
+    // TC -> O(N), SC -> O(N)
+    public int majorityElementII(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        int majorElem = nums.length / 2;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+            if (map.get(num) > majorElem) {
+                majorElem = num;
+                break;
+            }
+        }
+
+        return majorElem;
+    }
+
+    /* Boyer–Moore majority vote algorithm */
+    // TC -> O(N), SC -> O(1)
+    public int majorityElementIII(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        int majorElem = 0, count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                majorElem = num;
+            }
+            if (majorElem != num) {
+                count--;
+            } else {
+                count++;
+            }
+        }
+        return majorElem;
     }
 
 }
