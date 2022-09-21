d3e1423b24417e6876a5b4b06f8cfecefce3fce6
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-09-21 12:23:56 +0100

    add SetMismatch function

---
+++ leetcode/SetMismatch.java
@@ -0,0 +1,30 @@
+public class SetMismatch {
+
+    // TC -> O(N), SC -> O(1) (if we ignore the output data)
+    public int[] findErrorNums(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return new int[]{};
+        }
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            int idx = Math.abs(nums[i]) - 1;
+            if (nums[idx] < 0) {
+                result[0] = idx + 1;
+            } else {
+                nums[idx] = -1 * nums[idx];
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                nums[i] = -nums[i];
+            } else {
+                result[1] = i + 1;
+            }
+        }
+
+        return result;
+    }
+
+}
