3a6ec2648fe3303748a72d63132abaf599a8c148
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-09-22 17:40:49 +0100

    add hashtable impl for LinkedListCycle

--- leetcode/LinkedListCycle.java
+++ leetcode/LinkedListCycle.java
@@ -1,4 +1,42 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class LinkedListCycle {
+
+    // TC -> O(N), SC -> O(N)
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        Set<ListNode> set = new HashSet<>();
+        while (head != null) {
+            if (set.contains(head)) {
+                return true;
+            }
+            set.add(head);
+            head = head.next;
+        }
+        return false;
+    }
+
+    public boolean hasCycleII(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        Map<ListNode, Integer> map = new HashMap<>();
+        while (head != null) {
+            if (map.containsKey(head)) {
+                return true;
+            }
+            map.put(head, 1);
+            head = head.next;
+        }
+
+        return false;
+    }
 
     /**
      * Floyd’s Cycle-Finding Algorithm
@@ -6,7 +44,7 @@ public class LinkedListCycle {
      * tortoise and the hare algorithm
      */
     // TC -> O(N), SC -> O(1)
-    public boolean hasCycle(ListNode head) {
+    public boolean hasCycleIII(ListNode head) {
         if (head == null || head.next == null) {
             return false;
         }
