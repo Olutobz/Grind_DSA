aaca98c073ace7db78e1a493ae68758f2ae0eca2
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-09-22 14:30:46 +0100

    add SockMerchant function

---
+++ hackerrank/SockMerchant.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class SockMerchant {
+
+    // TC -> O(N), SC -> O(N)
+    public static int sockMerchant(List<Integer> arr) {
+        if (arr == null || arr.size() == 0) return -1;
+
+        Set<Integer> set = new HashSet<>();
+        int pairs = 0;
+        for (Integer elem : arr) {
+            if (set.contains(elem)) {
+                pairs++;
+                set.remove(elem);
+            } else {
+                set.add(elem);
+            }
+        }
+        return pairs;
+    }
+
+}
+
