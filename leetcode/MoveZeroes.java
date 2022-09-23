b46eb2872e7edf5f5a624f9b5d702b87f074a4be
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-09-23 22:47:21 +0100

    add function to move zeroes in an array

---
+++ leetcode/MoveZeroes.java
@@ -0,0 +1,16 @@
+public class MoveZeroes {
+
+    // TC -> O(N), SC -> O(1)
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index++] = nums[i];
+            }
+        }
+        while (index < nums.length) {
+            nums[index++] = 0;
+        }
+    }
+
+}
