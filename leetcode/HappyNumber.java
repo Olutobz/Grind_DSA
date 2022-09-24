cf4961e786d95868b8dfa280e02370e0a8d71ba9
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-09-24 12:51:40 +0100

    add HappyNumber function

---
+++ leetcode/HappyNumber.java
@@ -0,0 +1,36 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Write an algorithm to determine if a number n is happy.
+ * <p>
+ * A happy number is a number defined by the following process:
+ * <p>
+ * Starting with any positive integer, replace the number by the sum of the squares of its digits.
+ * Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+ * Those numbers for which this process ends in 1 are happy.
+ * Return true if n is a happy number, and false if not.
+ */
+
+public class HappyNumber {
+
+    // TC -> O(LogN), SC -> O(N)
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        while (n != 1 && !set.contains(n)) {
+            set.add(n);
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+
+    private int getNext(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int rem = n % 10;
+            sum += rem * rem;
+            n = n / 10;
+        }
+        return sum;
+    }
+}
