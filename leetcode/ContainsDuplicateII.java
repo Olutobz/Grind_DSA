db920debea272d70c87f85273d97cf2a3bcc95e8
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-09-27 21:50:45 +0100

    add ContainsDuplicateII function

---
+++ leetcode/ContainsDuplicateII.java
@@ -0,0 +1,49 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsDuplicateII {
+
+    // TC -> O(N^2), SC -> O(1)
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (nums == null || k < 0) {
+            throw new IllegalArgumentException("Input array is null");
+        }
+        if (nums.length <= 1 || k == 0) {
+            return false;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j] && j - i <= k) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // TC -> O(N), SC -> O(N)
+    public boolean containsNearbyDuplicateII(int[] nums, int k) {
+        if (nums == null || k < 0) {
+            throw new IllegalArgumentException("Input array is null");
+        }
+        if (nums.length <= 1 || k == 0) {
+            return false;
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                if (i - map.get(nums[i]) <= k) {
+                    return true;
+                } else {
+                    map.put(nums[i], i);
+                }
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return false;
+    }
+
+}
