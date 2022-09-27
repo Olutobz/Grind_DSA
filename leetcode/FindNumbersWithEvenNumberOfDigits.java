810653142edaf116aea357ee32cf884dd7aadcf6
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-09-27 17:32:22 +0100

    add a function to find numbers with even number of digits

---
+++ leetcode/FindNumbersWithEvenNumberOfDigits.java
@@ -0,0 +1,25 @@
+public class FindNumbersWithEvenNumberOfDigits {
+
+    public int findNumbers(int[] nums) {
+        // Check if the array is empty
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int digitCount = 0;
+            while (nums[i] != 0) {
+                nums[i] /= 10;
+                digitCount++;
+            }
+            if (digitCount % 2 == 0) {
+                count++;
+            }
+
+        }
+        return count;
+    }
+
+
+}
