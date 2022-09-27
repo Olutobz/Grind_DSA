086dc38426e75ead4471b93c94fe99e21c735675
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-09-27 23:47:27 +0100

    added another impl for ContainsDuplicateII function

--- leetcode/ContainsDuplicateII.java
+++ leetcode/ContainsDuplicateII.java
@@ -1,5 +1,7 @@
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class ContainsDuplicateII {
 
@@ -36,11 +38,31 @@ public class ContainsDuplicateII {
             if (map.containsKey(nums[i])) {
                 if (i - map.get(nums[i]) <= k) {
                     return true;
-                } else {
-                    map.put(nums[i], i);
                 }
             } else {
                 map.put(nums[i], i);
+            }
+        }
+        return false;
+    }
+
+    // TC -> O(N), SC -> O(min(N, K+1))
+    public boolean containsNearbyDuplicateIII(int[] nums, int k) {
+        if (nums == null || k < 0) {
+            throw new IllegalArgumentException("Input array is null");
+        }
+        if (nums.length <= 1 || k == 0) {
+            return false;
+        }
+
+        Set<Integer> window = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k) {
+                window.remove(nums[i - k - 1]);
+            }
+            if (!window.add(nums[i])) {
+                return true;
             }
         }
         return false;
