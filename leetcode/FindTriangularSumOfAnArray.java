d5517c0673d5c91f682d3124a0de7ca7cad8de56
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-09-28 08:31:26 +0100

    add recursive impl

--- leetcode/FindTriangularSumOfAnArray.java
+++ leetcode/FindTriangularSumOfAnArray.java
@@ -16,4 +16,21 @@ public class FindTriangularSumOfAnArray {
         return nums[0];
     }
 
+    // TC -> O(N^2), SC -> O(N) (Recursive call stack)
+    public int triangularSumII(int[] nums) {
+        return find(nums, nums.length);
+    }
+
+    public int find(int[] arr, int n) {
+        if (n == 1) {
+            return arr[0];
+        }
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            arr[i] = (arr[i] + arr[i + 1]) % 10;
+        }
+
+        return find(arr, n - 1);
+    }
+
 }
