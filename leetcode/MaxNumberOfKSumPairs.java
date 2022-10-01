57c5393513047c549d367e3e4f7b8996de9c7088
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-10-01 16:52:50 +0100

    add function to find MaxNumberOfKSumPairs

---
+++ leetcode/MaxNumberOfKSumPairs.java
@@ -0,0 +1,20 @@
+public class MaxNumberOfKSumPairs {
+    public int maxOperations(int[] nums, int k) {
+        if (nums == null || k <= 0) {
+            throw new IllegalArgumentException("Input array is null");
+        }
+        int len = nums.length, count = 0;
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (nums[j] == -1) continue;
+                if (nums[i] + nums[j] == k) {
+                    count++;
+                    nums[i] = -1;
+                    nums[j] = -1;
+                }
+            }
+        }
+        return count;
+    }
+}
+}
