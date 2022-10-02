61b9dd2c30ce0c949d0674a2aff0413f8eb5cd80
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-10-02 20:16:52 +0100

    add time & space complexity analysis

--- leetcode/FindNumbersWithEvenNumberOfDigits.java
+++ leetcode/FindNumbersWithEvenNumberOfDigits.java
@@ -1,10 +1,10 @@
 public class FindNumbersWithEvenNumberOfDigits {
 
+    // TC -> O(N), SC -> O(1)
     public int findNumbers(int[] nums) {
         if (nums.length == 0) {
             return 0;
         }
-
         int count = 0;
         for (int i = 0; i < nums.length; i++) {
             int digitCount = 0;
@@ -15,19 +15,18 @@ public class FindNumbersWithEvenNumberOfDigits {
             if (digitCount % 2 == 0) {
                 count++;
             }
-
         }
         return count;
     }
 
+    // TC -> O(N), SC -> O(1)
     public int findNumbersII(int[] nums) {
         if (nums.length == 0) {
             return 0;
         }
-
         int count = 0;
         for (int num : nums) {
-            if (String.valueOf(num).length() % 2 == 0) {
+            if (Integer.toString(num).length() % 2 == 0) {
                 count++;
             }
         }
