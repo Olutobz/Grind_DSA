7f4e9e51d468578fc70d0e54012c6d0b7f3736ab
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-10-03 21:46:44 +0100

    add a function to sort an array

---
+++ leetcode/SortAnArray.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class SortAnArray {
+
+    // TC -> O(NLogN), SC -> O(1)
+    public int[] sortArray(int[] nums) {
+        Arrays.sort(nums);
+        return nums;
+    }
+
+}
