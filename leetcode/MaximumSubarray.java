dd5ed9c03267d22b8258a0c226f92d4fb8303dde
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-10-05 00:09:27 +0100

    add a faster impl for finding max subarray

--- leetcode/MaximumSubarray.java
+++ leetcode/MaximumSubarray.java
@@ -12,4 +12,17 @@ public class MaximumSubarray {
         return maxSoFar;
     }
 
+    // TC -> O(N), SC -> O(1)
+    public int maxSubArrayII(int[] nums) {
+        int sum = 0, maxSoFar = Integer.MIN_VALUE;
+        for (int num : nums) {
+            sum += num;
+            maxSoFar = Math.max(maxSoFar, sum);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return maxSoFar;
+    }
+
 }
