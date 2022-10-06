5149fce562759e6efac370362a4406beb8ee2a2d
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-10-06 05:00:34 +0100

    added another impl for removing anagram from an array

--- leetcode/FindResultantArrayAfterRemovingAnagrams.java
+++ leetcode/FindResultantArrayAfterRemovingAnagrams.java
@@ -1,11 +1,49 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
 
 public class FindResultantArrayAfterRemovingAnagrams {
 
-    // TC -> O(N^2), SC -> O(N)
+    // TC -> O(K*NLogN), SC -> O(1)
     public List<String> removeAnagrams(String[] words) {
+        String prev = "";
+        List<String> res = new ArrayList<>();
+        if (words.length == 0) {
+            return res;
+        }
+
+        for (String word : words) {
+            char[] ch = word.toCharArray();
+            Arrays.sort(ch);
+            String curr = String.valueOf(ch);
+            if (!curr.equals(prev)) {
+                res.add(word);
+                prev = curr;
+            }
+        }
+        return res;
+    }
+
+    // TC -> O(N^2), SC -> O(1)
+    public List<String> removeAnagramsII(String[] words) {
+        List<String> res = new ArrayList<>();
+        if (words.length == 0) {
+            return res;
+        }
+        res.add(words[0]);
+
+        for (int i = 1; i < words.length; i++) {
+            if (!isAnagram(words[i], res.get(res.size() - 1))) {
+                res.add(words[i]);
+            }
+        }
+
+        return res;
+    }
+
+    // TC -> O(N^2), SC -> O(N)
+    public List<String> removeAnagramsIII(String[] words) {
         List<String> result = new ArrayList<>();
         Stack<String> stack = new Stack<>();
 
