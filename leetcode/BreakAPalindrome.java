10ab21a27f11203225fb2e49c67bb3f01aeed95f
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-10-08 05:23:24 +0100

    add a function to break a palindrome string

---
+++ leetcode/BreakAPalindrome.java
@@ -0,0 +1,21 @@
+public class BreakAPalindrome {
+
+    // TC -> O(N), SC -> O(N)
+    public String breakPalindrome(String palindrome) {
+        if (palindrome == null || palindrome.length() < 2) {
+            return "";
+        }
+
+        char[] arr = palindrome.toCharArray();
+
+        for (int i = 0; i < arr.length / 2; i++) {
+            if (arr[i] != 'a') {
+                arr[i] = 'a';
+                return String.valueOf(arr);
+            }
+        }
+
+        arr[arr.length - 1] = 'b';
+        return String.valueOf(arr);
+    }
+}
