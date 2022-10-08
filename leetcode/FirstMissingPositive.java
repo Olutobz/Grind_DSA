bfdae390829cb7e953c6116da545c1503d12df78
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-10-08 06:43:18 +0100

    handled edge cases

--- leetcode/FirstMissingPositive.java
+++ leetcode/FirstMissingPositive.java
@@ -4,6 +4,9 @@ public class FirstMissingPositive {
 
     // TC -> O(NlogN), SC -> O(sorting)
     public int firstMissingPositive(int[] nums) {
+        if (nums == null || nums.length == 1) {
+            return 1;
+        }
         Arrays.sort(nums);
         int res = 1;
         for (int num : nums) {
