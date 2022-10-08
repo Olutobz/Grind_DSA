551d25bf6bfa0b1fdec675cf1c163b80bae1cd69
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-10-08 11:08:02 +0100

    add func MaxNumberOfKSumPairs

--- leetcode/MaxNumberOfKSumPairs.java
+++ leetcode/MaxNumberOfKSumPairs.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class MaxNumberOfKSumPairs {
     public int maxOperations(int[] nums, int k) {
         if (nums == null || k <= 0) {
@@ -12,6 +15,22 @@ public class MaxNumberOfKSumPairs {
                     nums[i] = -1;
                     nums[j] = -1;
                 }
+            }
+        }
+        return count;
+    }
+
+    public int maxOperationsII(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        for (int num : nums) {
+            int res = k - num;
+            if (map.containsKey(res)) {
+                count++;
+                if (map.get(res) == 1) map.remove(res);
+                else map.put(res, map.get(res) - 1);
+            } else {
+                map.put(num, map.getOrDefault(num, 0) + 1);
             }
         }
         return count;
