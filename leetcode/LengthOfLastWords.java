392f26b84ef5078b4847dca234f88d605045182d
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-10-30 07:21:14 +0100

    add another impl for LengthOfLastWord

--- leetcode/LengthOfLastWords.java
+++ leetcode/LengthOfLastWords.java
@@ -7,4 +7,18 @@ public class LengthOfLastWords {
         return s.length();
     }
 
+    public int lengthOfLastWordII(String s) {
+        s = s.trim();
+        int count = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ' ') {
+                break;
+            } else {
+                count++;
+            }
+        }
+        return count;
+    }
+
 }
