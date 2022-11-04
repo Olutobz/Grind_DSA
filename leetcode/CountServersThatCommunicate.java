84c595b96b33aa2b75237de391059079fe2444f4
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-11-04 07:24:38 +0100

    add impl to countServers

--- leetcode/CountServersThatCommunicate.java
+++ leetcode/CountServersThatCommunicate.java
@@ -1,3 +1,35 @@
 public class CountServersThatCommunicate {
+    // TC -> O(N * M), SC -> O(N + M)
+    public int countServers(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        int numRows = grid.length;
+        int numCols = grid[0].length;
+        int[] countServersInRow = new int[numRows];
+        int[] countServersInCol = new int[numCols];
+        int totalServers = 0;
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                if (grid[i][j] == 1) {
+                    countServersInRow[i]++;
+                    countServersInCol[j]++;
+                    totalServers++;
+                }
+            }
+        }
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                if (grid[i][j] == 1) {
+                    if (countServersInRow[i] == 1 && countServersInCol[j] == 1) {
+                        totalServers--;
+                    }
+                }
+            }
+        }
+
+        return totalServers;
+    }
+
 
 }
