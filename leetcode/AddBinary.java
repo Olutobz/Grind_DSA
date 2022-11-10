8923ec4bc17d8fccafbc65c1fcdfac76939f88d1
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-11-10 10:08:50 +0100

    add AddBinary impl

--- leetcode/AddBinary.java
+++ leetcode/AddBinary.java
@@ -1,2 +1,29 @@
 public class AddBinary {
+    public String addBinary(String a, String b) {
+        if (a == null || a.isEmpty()) return b;
+        if (b == null || b.isEmpty()) return a;
+
+        StringBuilder res = new StringBuilder();
+        int i = a.length() - 1, j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += a.charAt(i) - '0';
+                i--;
+            }
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+                j--;
+            }
+            carry = sum > 1 ? 1 : 0;
+            res.append(sum % 2);
+
+        }
+
+        if (carry != 0) res.append(carry);
+        return res.reverse().toString();
+
+    }
 }
