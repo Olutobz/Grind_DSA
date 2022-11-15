7af66a8675f85982c2fcfe5e8b05b9a13b38b6bb
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-11-15 11:51:26 +0100

    update strStr func

--- leetcode/FindTheIndexOfTheFirstOccurrenceInAString.java
+++ leetcode/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -1,2 +1,19 @@
 public class FindTheIndexOfTheFirstOccurrenceInAString {
+    public int strStr(String haystack, String needle) {
+        if (needle.isEmpty()) return 0;
+        if (haystack.isEmpty() || haystack.length() < needle.length()) return -1;
+
+        for (int i = 0, j = 0; i < haystack.length(); i++) {
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                if (j == needle.length() - 1) return i - j;
+                else j++;
+            } else if (j > 0) {
+                i = i - j;
+                j = 0;
+            }
+        }
+
+        return -1;
+
+    }
 }
