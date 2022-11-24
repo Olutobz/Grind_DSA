9aa4387fe532d9aa65e138e14274da0b0d052bf0
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-11-24 20:50:36 +0100

    add func to find alternating chars

--- hackerrank/AlternatingCharacters.java
+++ hackerrank/AlternatingCharacters.java
@@ -1,2 +1,22 @@
+import java.util.HashMap;
+
 public class AlternatingCharacters {
+
+    public static int alternatingCharacters(String str) {
+        HashMap<Character, Integer> count = new HashMap<>();
+        int sumTotal = 0;
+
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == str.charAt(i - 1)) {
+                count.put(str.charAt(i - 1),
+                        count.containsKey(str.charAt(i - 1)) ? count.get(str.charAt(i - 1)) + 1 : 1);
+            }
+        }
+
+        for (int num : count.values()) {
+            sumTotal += num;
+        }
+        return sumTotal;
+    }
+
 }
