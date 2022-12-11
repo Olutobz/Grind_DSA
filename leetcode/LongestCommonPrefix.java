66b77d8daa51390a11349bb24df317267fbee5de
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-12-11 09:58:06 +0100

    add another impl to find the longest common prefix func

--- leetcode/LongestCommonPrefix.java
+++ leetcode/LongestCommonPrefix.java
@@ -19,4 +19,14 @@ public class LongestCommonPrefix {
         return c == 0 ? "" : first.substring(0, c);
     }
 
+    public String longestCommonPrefixII(String[] strs) {
+        String prefix = strs[0];
+        for (int index = 1; index < strs.length; index++) {
+            while (strs[index].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+        }
+        return prefix;
+    }
+
 }
