1fccea3ad1e8e2e1043f8c46d087848378680805
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-12-12 10:00:37 +0100

    add time & space complexities

--- leetcode/LongestCommonPrefix.java
+++ leetcode/LongestCommonPrefix.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 
 public class LongestCommonPrefix {
 
+    // TC -> O(NLogN), SC -> O(max(M,N))
     public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0)
             return "";
@@ -19,6 +20,7 @@ public class LongestCommonPrefix {
         return c == 0 ? "" : first.substring(0, c);
     }
 
+    // TC -> O(NM), SC -> O(1)
     public String longestCommonPrefixII(String[] strs) {
         String prefix = strs[0];
         for (int index = 1; index < strs.length; index++) {
