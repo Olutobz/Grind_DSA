071d1e7843e1c2cd628200cebde1ddd2e2a2c6e5
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-12-13 10:02:31 +0100

    reformat code and correct space complexity

--- leetcode/LongestCommonPrefix.java
+++ leetcode/LongestCommonPrefix.java
@@ -2,20 +2,17 @@ import java.util.Arrays;
 
 public class LongestCommonPrefix {
 
-    // TC -> O(NLogN), SC -> O(max(M,N))
+    // TC -> O(NLogN), SC -> O(1)
     public String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0)
-            return "";
+        if (strs == null || strs.length == 0) return "";
 
         Arrays.sort(strs);
         String first = strs[0];
         String last = strs[strs.length - 1];
         int c = 0;
         while (c < first.length()) {
-            if (first.charAt(c) == last.charAt(c))
-                c++;
-            else
-                break;
+            if (first.charAt(c) == last.charAt(c)) c++;
+            else break;
         }
         return c == 0 ? "" : first.substring(0, c);
     }
