0b6076ee2dbb58336d1fdf3fbb5ffc5250d638b0
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2022-12-21 22:45:30 +0100

    include comment

--- leetcode/ParkingSystem.java
+++ leetcode/ParkingSystem.java
@@ -1,2 +1,9 @@
+
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * boolean param_1 = obj.addCar(carType);
+ */
 public class ParkingSystem {
 }
