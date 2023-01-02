56b5496eb5c9af1ae41ca348ed1c069dbfd1861a
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-01-02 11:00:11 +0100

    add other impl for shuffle func

--- leetcode/ShuffleTheArray.java
+++ leetcode/ShuffleTheArray.java
@@ -13,4 +13,27 @@ public class ShuffleTheArray {
         return nums;
     }
 
+    // TC -> O(N), SC -> O(N)
+    public int[] shuffleII(int[] nums, int n) {
+        int[] res = new int[n * 2];
+        int idx = 0;
+        for (int i = 0, j = n; idx < res.length; i++, j++) {
+            res[idx++] = nums[i];
+            res[idx++] = nums[j];
+        }
+
+        return res;
+    }
+
+    // TC -> O(N), SC -> O(N)
+    public int[] shuffleIII(int[] nums, int n) {
+        int[] res = new int[n * 2];
+        for (int i = 0; i < nums.length; i++) {
+            res[2 * i] = nums[i];
+            res[2 * i + 1] = nums[i];
+        }
+
+        return res;
+    }
+
 }
