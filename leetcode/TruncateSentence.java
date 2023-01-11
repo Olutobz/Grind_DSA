0e8ec4fd98a8612168b17e4d0cd188f8c69e143e
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-01-11 08:45:28 +0100

    modified TruncateSentence class

--- leetcode/TruncateSentence.java
+++ leetcode/TruncateSentence.java
@@ -1,15 +1,40 @@
 public class TruncateSentence {
 
-    // TC -> O(N), SC -> O(1)
+    public String truncateSentenceIII(String s, int k) {
+        if (s == null || s.length() == 0) return "";
+        int count = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) == ' ') {
+                count++;
+                if (count == k) {
+                    return s.substring(0, i);
+                }
+            }
+        }
+        return s;
+    }
+
+    // TC -> O(N), SC -> O(N)
     public String truncateSentence(String s, int k) {
         if (s == null || s.length() == 0) return "";
         String[] words = s.split(" ");
-        StringBuilder str = new StringBuilder();
+        StringBuilder res = new StringBuilder();
         for (int i = 0; i < k; i++) {
-            str.append(words[i]).append(' ');
+            res.append(words[i]).append(' ');
         }
-
-        return str.toString().trim();
-
+        return res.toString().trim();
     }
+
+    public String truncateSentenceII(String s, int k) {
+        if (s == null || s.length() == 0) return "";
+        StringBuilder res = new StringBuilder();
+        int spaceCount = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') spaceCount++;
+            if (spaceCount == k) break;
+            res.append(s.charAt(i));
+        }
+        return res.toString();
+    }
+
 }
