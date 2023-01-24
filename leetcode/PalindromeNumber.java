82eead1f8aaee26082d9def2bfdb45eebd9cce21
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-01-24 14:24:36 +0100

    add isPalindrome func

--- leetcode/PalindromeNumber.java
+++ leetcode/PalindromeNumber.java
@@ -1,3 +1,15 @@
 public class PalindromeNumber {
+    public boolean isPalindrome(int x) {
+        String temp = Integer.toString(x);
+        int i = 0, j = temp.length() - 1;
 
+        while (i < j) {
+            if (temp.charAt(i) != temp.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
 }
