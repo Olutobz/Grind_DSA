49f62bf674584cc3fd694de27fbaeaf5e726be91
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-01-26 14:29:51 +0100

    modified PalindromeNumber class

--- leetcode/PalindromeNumber.java
+++ leetcode/PalindromeNumber.java
@@ -6,11 +6,9 @@ public class PalindromeNumber {
         int i = 0, j = temp.length() - 1;
 
         while (i < j) {
-            if (temp.charAt(i) != temp.charAt(j)) {
+            if (temp.charAt(i++) != temp.charAt(j--)) {
                 return false;
             }
-            i++;
-            j--;
         }
         return true;
     }
