c390e0d4d51a4d33c010ad0c3e487d177539a1fe
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-01-27 14:32:02 +0100

    add edge case for isPalindrome func

--- leetcode/PalindromeNumber.java
+++ leetcode/PalindromeNumber.java
@@ -2,6 +2,7 @@ public class PalindromeNumber {
 
     // TC -> O(N), SC -> O(1);
     public boolean isPalindrome(int x) {
+        if (x < 0) return false;
         String temp = Integer.toString(x);
         int i = 0, j = temp.length() - 1;
 
