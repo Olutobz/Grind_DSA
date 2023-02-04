f13adad76ce00eb33de39f628b2b50f7b8d6fb6c
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-02-04 12:32:09 +0100

    add stringConstruction impl

--- hackerrank/StringConstruction.java
+++ hackerrank/StringConstruction.java
@@ -1,2 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class StringConstruction {
+
+    // TC -> O(N), SC -> O(N)
+    public static int stringConstruction(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        return map.size();
+    }
 }
