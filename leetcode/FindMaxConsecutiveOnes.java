148a57d8ea8e6cadf0360783c04917f78f5341cf
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-02-05 20:16:31 +0100

    throw an exception for edge case

--- leetcode/FindMaxConsecutiveOnes.java
+++ leetcode/FindMaxConsecutiveOnes.java
@@ -1,13 +1,15 @@
 public class FindMaxConsecutiveOnes {
     // Given a binary array nums, return the maximum number of consecutive 1's in the array.
     public int findMaxConsecutiveOnes(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            throw new IllegalArgumentException("Input array is null");
+        }
+
         int count = 0, max = 0;
         for (int num : nums) {
-            if (num == 0) {
-                count = 0;
-            } else if (num == 1) {
+            if (num == 1) {
                 count++;
-            }
+            } else count = 0;
             max = Math.max(count, max);
         }
         return max;
