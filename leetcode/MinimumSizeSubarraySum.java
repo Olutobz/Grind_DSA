af353f257acf246e6f2d08d5aa85f8bfe51f1aca
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-02-07 20:28:34 +0100

    add impl for func minSubArrayLen

--- leetcode/MinimumSizeSubarraySum.java
+++ leetcode/MinimumSizeSubarraySum.java
@@ -1,2 +1,19 @@
 public class MinimumSizeSubarraySum {
+
+    // TC -> O(N), SC -> O(1)
+    public int minSubArrayLen(int target, int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int min = Integer.MAX_VALUE;
+        int sum = 0;
+
+        for (int windowEnd = 0, windowStart = 0; windowEnd < nums.length; windowEnd++) {
+            sum += nums[windowEnd];
+            while (sum >= target) {
+                min = Math.min(min, windowEnd - windowStart + 1);
+                sum -= nums[windowStart];
+                windowStart++;
+            }
+        }
+        return min == Integer.MAX_VALUE ? 0 : min;
+    }
 }
