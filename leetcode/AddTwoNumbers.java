f9a2e9d5127c2e589d2291d6600821d44d580baa
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-02-08 10:01:12 +0100

    add singly-linked list definition to AddTwoNumbers class

--- leetcode/AddTwoNumbers.java
+++ leetcode/AddTwoNumbers.java
@@ -1,2 +1,21 @@
 public class AddTwoNumbers {
+
+    // Definition for singly-linked list.
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
 }
