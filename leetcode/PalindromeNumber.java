359e9e989e3e57110908e5100f41cbb69fc58d63
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-02-09 08:02:11 +0100

    correct space complexity analysis

--- leetcode/PalindromeNumber.java
+++ leetcode/PalindromeNumber.java
@@ -1,11 +1,10 @@
 public class PalindromeNumber {
 
-    // TC -> O(N), SC -> O(1);
+    // TC -> O(N), SC -> O(N);
     public boolean isPalindrome(int x) {
         if (x < 0) return false;
         String temp = Integer.toString(x);
         int i = 0, j = temp.length() - 1;
-
         while (i < j) {
             if (temp.charAt(i++) != temp.charAt(j--)) {
                 return false;
