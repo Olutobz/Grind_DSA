a239ccae5f0ad3eb024a72acaf18fe3902cbe4c1
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-02-09 08:44:11 +0100

    add a better impl to find the palindrome number

--- leetcode/PalindromeNumber.java
+++ leetcode/PalindromeNumber.java
@@ -12,4 +12,18 @@ public class PalindromeNumber {
         }
         return true;
     }
+
+    // TC -> O(LogN), SC -> O(1);
+    public boolean isPalindromeII(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+        int res = 0;
+
+        while (x > res) {
+            res = res * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == res || x == res / 10;
+    }
+
 }
