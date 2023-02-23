b2dedbef717ee48f94f767218f6f9a38d3eb642e
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-02-23 10:39:43 +0100

    add function to compress an array of chars

---
+++ leetcode/StringCompression.java
@@ -0,0 +1,27 @@
+public class StringCompression {
+
+    // TC -> O(N), SC -> O(1)
+    public int compress(char[] chars) {
+        if (chars == null || chars.length == 0) {
+            return 0;
+        }
+
+        int left = 0, right = 0, idx = 0;
+        while (left < chars.length) {
+            while (right < chars.length && chars[right] == chars[left]) {
+                right++;
+            }
+            int freq = right - left;
+            chars[idx++] = chars[left];
+            if (freq > 1) {
+                String str = String.valueOf(freq);
+                for (int i = 0; i < str.length(); i++) {
+                    chars[idx++] = str.charAt(i);
+                }
+            }
+            left = right;
+        }
+
+        return idx;
+    }
+}
