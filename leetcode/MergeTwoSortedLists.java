cb3364c54637cffa6d899f3c4cc452881bd58998
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-02-25 07:40:23 +0100

    add func to merge two sorted singly linkedlist

---
+++ leetcode/MergeTwoSortedLists.java
@@ -0,0 +1,46 @@
+public class MergeTwoSortedLists {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    // TC -> O(N + M), SC -> O(1)
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        else if (l2 == null) return l1;
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        curr.next = l1 == null ? l2 : l1;
+        return dummy.next;
+    }
+
+    // TC -> O(N + M), SC -> O(N + M)
+    public ListNode mergeTwoListsII(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+
+}
