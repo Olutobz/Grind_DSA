6dfa0a1f750bd20e5f005aa1fd8c645593001c07
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-03-06 20:19:57 +0100

    create class to find maximum depth of nested parenthesis in a string

---
+++ leetcode/MaximumNestingDepthOfTheParentheses.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+public class MaximumNestingDepthOfTheParentheses {
+
+    // TC -> O(N), SC -> O(1)
+    public int maxDepth(String s) {
+        if (s == null || s.length() == 0) return 0;
+        int count = 0;
+        int max = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                count++;
+            } else if (s.charAt(i) == ')') {
+                count--;
+            }
+            max = Math.max(max, count);
+        }
+        return max;
+    }
+}
---
+++ random/FindMaximumDepthOfNestedParenthesisInAString.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+public class FindMaximumDepthOfNestedParenthesisInAString {
+
+    /* Input : S = "(a(b)(c)(d(e(f)g)h)I(j(k)l)m)";
+       Output : 4
+    * */
+
+    // TC -> O(N), SC -> O(N)
+    public static int maxDepth(String s) {
+        int count = 0;
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(')
+                st.push(i);//pushing the bracket in the stack
+            else if (s.charAt(i) == ')') {
+                count = Math.max(count, st.size());
+                st.pop();
+            }
+        }
+        return count;
+    }
+}
