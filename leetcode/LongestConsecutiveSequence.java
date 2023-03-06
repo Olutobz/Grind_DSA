b71c9015791420e49f795230b4b2342a46266242
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-03-06 08:57:04 +0100

    modified LongestConsecutiveSequence class

--- leetcode/LongestConsecutiveSequence.java
+++ leetcode/LongestConsecutiveSequence.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -26,6 +27,46 @@ public class LongestConsecutiveSequence {
             }
         }
 
+        return longestStreak;
+    }
+
+    // TC -> O(N), SC -> O(N)
+    public int longestConsecutiveII(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+
+        Set<Integer> set = new HashSet<>();
+        for (int i : nums) set.add(i);
+
+        int ans = 0;
+        for (int num : nums) {
+            int left = num - 1;
+            int right = num + 1;
+            while (set.remove(left)) left--;
+            while (set.remove(right)) right++;
+            ans = Math.max(ans, right - left - 1);
+            if (set.isEmpty()) return ans;//save time if there are items in nums, but no item in hashset.
+        }
+        return ans;
+    }
+
+    // TC -> O(NLogN), SC -> O(1)
+    public int longestConsecutiveIII(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        Arrays.sort(nums);
+        int curr = nums[0];
+        int count = 1;
+        int longestStreak = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) continue;
+            if (nums[i] == curr + 1) {
+                curr++;
+                count++;
+                longestStreak = Math.max(longestStreak, count);
+            } else {
+                curr = nums[i];
+                count = 1;
+            }
+        }
         return longestStreak;
     }
 
