2c2e633ca07a0b9da22b33158c602c99b163aade
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-03-08 00:18:58 +0100

    add time and space complexities

--- cr/ImplementQueueUsingStacks.java
+++ cr/ImplementQueueUsingStacks.java
@@ -2,41 +2,49 @@ import java.util.Stack;
 
 public class ImplementQueueUsingStacks {
     static class MyQueue {
-        Stack<Integer> s1;
-        Stack<Integer> s2;
+
+        Stack<Integer> stack;
+        Stack<Integer> tstack;
 
         public MyQueue() {
-            s1 = new Stack<>();
-            s2 = new Stack<>();
+            stack = new Stack<>();
+            tstack = new Stack<>();
         }
 
+        // TC -> O(1), SC -> O(1)
         // Pushes element x to the back of the queue.
         public void push(int x) {
-            while (!s2.isEmpty())
-                s1.push(s2.pop());
-
-            s1.push(x);
+            stack.push(x);
         }
 
+        // TC -> O(1), SC -> O(1)
         // Removes the element from in front of queue.
         public int pop() {
-            while (!s1.isEmpty())
-                s2.push(s1.pop());
-
-            return s2.pop();
+            if (tstack.isEmpty()) {
+                while (!stack.isEmpty()) {
+                    int x = stack.pop();
+                    tstack.push(x);
+                }
+            }
+            return tstack.pop();
         }
 
+        // TC -> O(1), SC -> O(1)
         // Get the front element.
         public int peek() {
-            while (!s1.isEmpty())
-                s2.push(s1.pop());
-
-            return s2.peek();
+            if (tstack.isEmpty()) {
+                while (!stack.isEmpty()) {
+                    int x = stack.pop();
+                    tstack.push(x);
+                }
+            }
+            return tstack.peek();
         }
 
+        // TC -> O(1), SC -> O(1)
         // Return whether the queue is empty.
         public boolean empty() {
-            return s1.isEmpty() && s2.isEmpty();
+            return stack.isEmpty() && tstack.isEmpty();
         }
     }
 
