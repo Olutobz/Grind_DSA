090a1c1cee5b5e54073e6a11fa8fc24ca3b35a26
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-03-08 11:28:54 +0100

    create class MinStack

---
+++ leetcode/MinStack.java
@@ -0,0 +1,84 @@
+import java.util.Stack;
+
+class MinStack {
+    static class Node {
+        int val;
+        int min;
+        Node next;
+
+        public Node(int val) {
+            this.val = val;
+        }
+
+        public Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+
+    Node head;
+
+    public MinStack() {
+
+    }
+
+    public void push(int val) {
+        if (head == null) {
+            head = new Node(val, val, null);
+        } else {
+            head = new Node(val, Math.min(head.min, val), head);
+        }
+
+    }
+
+    public void pop() {
+        if (head != null) {
+            head = head.next;
+        }
+    }
+
+    public int top() {
+        if (head != null) {
+            return head.val;
+        }
+        return -1;
+    }
+
+    public int getMin() {
+        if (head != null) {
+            return head.min;
+        }
+        return -1;
+    }
+
+
+    static class MinStack2 {
+        Stack<Integer> stack = new Stack<>();
+        int min = Integer.MAX_VALUE;
+
+        public void push(int x) {
+            if (x <= min) {
+                stack.push(min);
+                min = x;
+            }
+            stack.push(x);
+        }
+
+        public void pop() {
+            if (stack.peek() == min) {
+                stack.pop();
+                min = stack.pop();
+            } else stack.pop();
+        }
+
+        public int top() {
+            return stack.peek();
+        }
+
+        public int getMin() {
+            return min;
+        }
+    }
+
+}
