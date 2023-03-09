2e3766d2b06de8562c6ee3ba2dcee83b5fe06599
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-03-09 19:10:39 +0100

    add func to find the intersection of two linkedlist

---
+++ leetcode/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,34 @@
+public class IntersectionOfTwoLinkedLists {
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {
+        }
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+
+        ListNode a = headA;
+        ListNode b = headB;
+
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+
+        return a;
+    }
+}
