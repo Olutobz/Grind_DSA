dd551c22cd0bbd5d2e1ab7a042e4d346c91f11be
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-03-12 12:29:57 +0100

    --amend

--- leetcode/LargestNumberAtLeastTwiceOfOthers.java
+++ leetcode/LargestNumberAtLeastTwiceOfOthers.java
@@ -2,7 +2,7 @@ public class LargestNumberAtLeastTwiceOfOthers {
 
     // TC -> O(N), SC -> O(1)
     public int dominantIndex(int[] nums) {
-        if (nums == null || nums.length == 0) return 0;
+        if (nums == null || nums.length == 0) return -1;
 
         int max1 = Integer.MIN_VALUE;
         int max2 = Integer.MAX_VALUE;
@@ -20,4 +20,24 @@ public class LargestNumberAtLeastTwiceOfOthers {
 
         return max1 >= max2 * 2 ? idx : -1;
     }
+
+    // TC -> O(N), SC -> O(1)
+    public int dominantIndexII(int[] nums) {
+        if (nums == null || nums.length == 0) return -1;
+
+        int maxElem = Integer.MIN_VALUE, maxIndex = -1, count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > maxElem) {
+                maxElem = nums[i];
+                maxIndex = i;
+            }
+        }
+        for (int num : nums) {
+            if (maxElem >= num * 2) count++;
+        }
+
+        return count == nums.length - 1 ? maxIndex : -1;
+    }
+
 }
