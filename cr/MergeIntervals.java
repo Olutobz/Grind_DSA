c909d09aafd90d7e0ca08f86b27165b0c3801e0a
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-03-14 12:01:23 +0100

    modified func to find merge intervals

--- cr/MergeIntervals.java
+++ cr/MergeIntervals.java
@@ -2,29 +2,60 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MergeInterval {
+public class MergeIntervals {
 
     public int[][] merge(int[][] intervals) {
-        List<int[]> answer = new ArrayList<>();
-
-        if (intervals.length != 0 || intervals != null) {
-            Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-
-            int start = intervals[0][0];
-            int end = intervals[0][1];
-            for (int[] i : intervals) {
-                if (i[0] <= end) {
-                    end = Math.max(end, i[1]);
-                } else {
-                    answer.add(new int[]{start, end});
-                    start = i[0];
-                    end = i[1];
-                }
-            }
-            answer.add(new int[]{start, end});
-
+        if (intervals == null || intervals.length == 0) {
+            return new int[0][0];
         }
 
-        return answer.toArray(new int[0][]);
+        int n = intervals.length;
+        int[] starts = new int[n];
+        int[] ends = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            starts[i] = intervals[i][0];
+            ends[i] = intervals[i][1];
+        }
+
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+        List<int[]> res = new ArrayList<>();
+
+        // Two pointers
+        int endIndex = 0, startIndex = 0;
+        while (endIndex < n) {
+            if (endIndex == n - 1 || starts[endIndex + 1] > ends[endIndex]) {
+                res.add(new int[]{starts[startIndex], ends[endIndex]});
+                startIndex = endIndex + 1;
+            }
+            endIndex++;
+        }
+
+        return res.toArray(new int[res.size()][]);
     }
+
+    public List<int[]> merge(List<int[]> intervals) {
+        // sort start&end
+        int n = intervals.size();
+        int[] starts = new int[n];
+        int[] ends = new int[n];
+        for (int i = 0; i < n; i++) {
+            starts[i] = intervals.get(i)[0];
+            ends[i] = intervals.get(i)[1];
+        }
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+
+        List<int[]> res = new ArrayList<>();
+        for (int i = 0, j = 0; i < n; i++) {
+            // j is start of interval.
+            if (i == n - 1 || starts[i + 1] > ends[i]) {
+                res.add(new int[]{starts[j], ends[i]});
+                j = i + 1;
+            }
+        }
+        return res;
+    }
+
 }
