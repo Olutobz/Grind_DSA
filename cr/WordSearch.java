994fa3907621615eb059b80a6efc2225dea1e4dc
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-03-15 13:00:59 +0100

    add WordSearch class

---
+++ cr/WordSearch.java
@@ -0,0 +1,33 @@
+public class WordSearch {
+
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (word.charAt(0) == board[i][j] && exist(board, i, j, word, 0)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean exist(char[][] board, int i, int j, String word, int idx) {
+        if (idx == word.length()) return true;
+
+        if (i < 0 || j < 0 || i > board.length - 1 || j > board[0].length - 1
+                || board[i][j] != word.charAt(idx)) {
+            return false;
+        }
+
+        board[i][j] = '*';
+        boolean result = exist(board, i - 1, j, word, idx + 1) ||
+                exist(board, i, j - 1, word, idx + 1) ||
+                exist(board, i, j + 1, word, idx + 1) ||
+                exist(board, i + 1, j, word, idx + 1);
+
+        board[i][j] = word.charAt(idx); // backtrack
+        return result;
+    }
+
+}
