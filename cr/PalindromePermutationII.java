56c9031482d9fcfafe55fd722e131ae1e36bfe14
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-03-15 15:24:42 +0100

    add additional impl to PalindromePermutationII

--- cr/PalindromePermutationII.java
+++ cr/PalindromePermutationII.java
@@ -1,5 +1,4 @@
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class PalindromePermutationII {
 
@@ -16,7 +15,79 @@ public class PalindromePermutationII {
      * Output: []
      */
 
+    // TC -> O(N^2), SC -> O(N)
     public List<String> generatePalindromes(String s) {
+
+        List<String> result = new ArrayList<>();
+
+        int odd = 0;
+
+        // char => its count
+        Map<Character, Integer> map = new HashMap<>();
+
+        // step 1. build character count map and count odds
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            map.put(c, 1 + map.getOrDefault(c, 0));
+            odd += map.get(c) % 2 != 0 ? 1 : -1;
+        }
+
+        // cannot form any palindromic string
+        if (odd > 1) {
+            return result;
+        }
+
+        // step 2. add half count of each character to list
+        StringBuilder mid = new StringBuilder();
+        List<Character> charList = new ArrayList<>();
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            char key = entry.getKey();
+            int val = entry.getValue();
+
+            if (val % 2 != 0) { // odd count char
+                mid.append(key);
+            }
+
+            for (int i = 0; i < val / 2; i++) {
+                charList.add(key);
+            }
+        }
+
+        // step 3. generate all the permutations
+        getPerm(charList, mid.toString(), new boolean[charList.size()], new StringBuilder(), result);
+
+        return result;
+    }
+
+    // generate all unique permutation from list, sb: cur_res
+    void getPerm(List<Character> list, String midChar, boolean[] used, StringBuilder sb, List<String> result) {
+        if (sb.length() == list.size()) {
+            // form the palindromic string
+            result.add(sb + midChar + sb.reverse());
+            sb.reverse(); // restore
+            return;
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            // avoid duplication
+            // i-1 must already checked, so skip used[i - 1]==false
+            if (i > 0 && list.get(i) == list.get(i - 1) && !used[i - 1]) {
+                continue;
+            }
+
+            if (!used[i]) {
+                used[i] = true;
+                sb.append(list.get(i));
+                // recursion
+                getPerm(list, midChar, used, sb, result);
+                // backtracking
+                used[i] = false;
+                sb.deleteCharAt(sb.length() - 1);
+            }
+        }
+    }
+
+    public List<String> generatePalindromesII(String s) {
         int[] charInts = new int[256];
 
         for (int i = 0; i < s.length(); i++) {
