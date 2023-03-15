de073443825a7a9496015c89cb70ff5b8f759f16
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-03-15 15:23:51 +0100

    add time and space complexity analysis to WordSearch

--- cr/WordSearch.java
+++ cr/WordSearch.java
@@ -1,5 +1,6 @@
 public class WordSearch {
 
+    // TC ->O(M * N * 3^L),  SC -> O(L) where M*N is the size of the board
     public boolean exist(char[][] board, String word) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
