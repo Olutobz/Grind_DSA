a6b4d433a91c83c06da525fe796d23c9c5a58ce3
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-03-15 15:27:45 +0100

    add extra impl for WildcardMatching class

--- cr/WildcardMatching.java
+++ cr/WildcardMatching.java
@@ -5,8 +5,37 @@ public class WildcardMatching {
        at least one pointer advance one step.
      */
 
+
+    // TC -> O(M * N), SC -> O(N + M) where, M = length(str) and N = length(pattern)
+    public boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        char[] sc = s.toCharArray();
+        char[] pc = p.toCharArray();
+
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+
+        for (int j = 1; j < n + 1; j++) {
+            if (pc[j - 1] == '*') dp[0][j] = dp[0][j - 1];
+        }
+
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (pc[j - 1] == sc[i - 1] || pc[j - 1] == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (pc[j - 1] == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
     // TC -> O(M * N), SC -> O(1) where, M = length(str) and N = length(pattern)
-    public boolean isMatch(String str, String pattern) {
+    public boolean isMatchII(String str, String pattern) {
         int i = 0, j = 0;
         int match = 0, starIdx = -1;
 
