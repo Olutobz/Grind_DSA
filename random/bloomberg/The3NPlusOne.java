260380057d3916326750a4f93a0e7060bbced4f1
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2023-12-03 12:16:50 +0100

    Solve 3N + 1 problem

---
+++ random/bloomberg/The3NPlusOne.java
@@ -0,0 +1,27 @@
+package bloomberg;
+
+import java.util.Scanner;
+
+/**
+ * Created by Onikoyi Damola Olutoba
+ * DATE: 03 December 2023
+ * EMAIL: damexxey94@gmail.com
+ */
+public class The3NPlusOne {
+
+    private void ThreeNPlus1() {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int res = 1;
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = (3 * n) + 1;
+            }
+            res++;
+        }
+        System.out.println(res);
+    }
+
+}
