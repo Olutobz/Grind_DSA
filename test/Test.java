6a5d9e6ce2d98822c88833ecad1b721ff6017ca2
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-05-10 01:30:42 +0100

    refactor function to print 2d array content

--- test/Test.java
+++ test/Test.java
@@ -3,37 +3,15 @@ import java.util.Arrays;
 
 public class Test {
 
-    public static void printArray(int[][] arr) {
-
-        for (int[] num : arr) {
-            System.out.println(Arrays.toString(num));
-        }
-
-        for (int[] nums : arr) {
-            for (int j = 0; nums != null && j < nums.length; j++) {
-                System.out.print(nums[j] + " ");
-            }
-            System.out.println();
-        }
-        /*  for (int[] a : arr) {
-            for (int val : a) {
-                System.out.print(val + " ");
-            }
-            System.out.println();
-            }
-        * */
-
-    }
-
     public static void main(String[] args) {
         int[][] a = new int[2][4];
-        printArray(a);
+        print2DArrayContents(a);
 
         int[][] b = new int[2][];
-        printArray(b);
+        print2DArrayContents(b);
         b[0] = new int[2];
         b[1] = new int[3];
-        printArray(b);
+        print2DArrayContents(b);
 
         int[] nums = {1, 1, 3, 2, 2, 3, 2, 2};
         int[] buc = new int[nums.length];
@@ -43,5 +21,21 @@ public class Test {
         }
     }
 
+    private static void print2DArrayContents(int[][] arr) {
+        if (arr == null || arr.length == 0)
+            return;
 
+        for (int[] num : arr) {
+            System.out.println(Arrays.toString(num));
+        }
+
+        for (int[] nums : arr) {
+            if (nums != null) {
+                for (int num : nums) {
+                    System.out.print(num + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
