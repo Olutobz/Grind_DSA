6bb19f3dbbf7e356a691cdb99419cbc54499c58b
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-05-26 15:33:24 +0100

    modify class FindTheIndexOfTheFirstOccurrenceInAString

--- leetcode/FindTheIndexOfTheFirstOccurrenceInAString.java
+++ leetcode/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -1,13 +1,30 @@
+
 public class FindTheIndexOfTheFirstOccurrenceInAString {
+
+    // TC -> O(MN), SC -> O(1)
+    public int strStr(String haystack, String needle) {
+        if (needle == null || haystack == null || haystack.length() < needle.length()) {
+            return -1;
+        }
+        for (int i = 0, j = 0; i < haystack.length(); i++) {
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                if (j == needle.length() - 1) return i - j;
+                else j++;
+            } else if (j > 0) {
+                i = i - j;
+                j = 0;
+            }
+        }
+        return -1;
+    }
 
     // TC -> O(MN), SC -> O(1)
     public int strStrII(String haystack, String needle) {
         if (needle == null || haystack == null || haystack.length() < needle.length()) {
             return -1;
         }
-
-        int m = needle.length(), n = haystack.length();
-
+        int m = needle.length();
+        int n = haystack.length();
         for (int windowStart = 0; windowStart <= n - m; windowStart++) {
             for (int i = 0; i < m; i++) {
                 if (needle.charAt(i) != haystack.charAt(windowStart + i)) {
@@ -18,27 +35,6 @@ public class FindTheIndexOfTheFirstOccurrenceInAString {
                 }
             }
         }
-
         return -1;
-    }
-
-    // TC -> O(MN), SC -> O(1)
-    public int strStr(String haystack, String needle) {
-        if (needle == null || haystack == null || haystack.length() < needle.length()) {
-            return -1;
-        }
-
-        for (int i = 0, j = 0; i < haystack.length(); i++) {
-            if (haystack.charAt(i) == needle.charAt(j)) {
-                if (j == needle.length() - 1) return i - j;
-                else j++;
-            } else if (j > 0) {
-                i = i - j;
-                j = 0;
-            }
-        }
-
-        return -1;
-
     }
 }
