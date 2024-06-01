733ec2f97df8106b7fe120a0c707b5ccc0fdd9cc
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-06-01 04:03:35 +0100

    modify function to reverse an integer

--- leetcode/ReverseInteger.java
+++ leetcode/ReverseInteger.java
@@ -1,18 +1,20 @@
+/*
+* Input: x = 123
+  Output: 321
+*/
 public class ReverseInteger {
-
     // TC -> O(log(x)), SC -> O(1)
     public int reverse(int x) {
         int result = 0;
         while (x != 0) {
-            int tail = x % 10;
-            int newResult = result * 10 + tail;
-            if ((newResult - tail) / 10 != result) {
+            int pop = x % 10;
+            x /= 10;
+            int temp = result * 10 + pop;
+            if (result > Integer.MAX_VALUE / 10 || result < Integer.MIN_VALUE / 10) {
                 return 0;
             }
-            result = newResult;
-            x = x / 10;
+            result = temp;
         }
-
         return result;
     }
 }
