27158a6dde59c455efbe8eb8fdb74d2117e59dcf
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-06-01 16:38:52 +0100

    modify comment

--- leetcode/ReverseInteger.java
+++ leetcode/ReverseInteger.java
@@ -1,20 +1,20 @@
 /*
-* Input: x = 123
-  Output: 321
-*/
+ * Input: x = 123 => Output: 321
+ * Input: x = -123 => Output: -321
+ */
 public class ReverseInteger {
     // TC -> O(log(x)), SC -> O(1)
     public int reverse(int x) {
-        int result = 0;
+        int rev = 0;
         while (x != 0) {
             int pop = x % 10;
             x /= 10;
-            int temp = result * 10 + pop;
-            if (result > Integer.MAX_VALUE / 10 || result < Integer.MIN_VALUE / 10) {
+            int temp = rev * 10 + pop;
+            if (rev > Integer.MAX_VALUE / 10 || rev < Integer.MIN_VALUE / 10) {
                 return 0;
             }
-            result = temp;
+            rev = temp;
         }
-        return result;
+        return rev;
     }
 }
