de1862875ad05e406dc4ddde1f960256879628a4
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-06-02 01:03:18 +0100

    add pattern number 3

--- striver/pattern/Patterns.java
+++ striver/pattern/Patterns.java
@@ -12,12 +12,15 @@ public class Patterns {
         System.out.println("______________________________________");
         pattern2();
         System.out.println("______________________________________");
+        pattern3();
+        System.out.println("______________________________________");
+
     }
 
     static void pattern1() {
-        for (int i = 0; i < 3; ++i) {
-            for (int j = 0; j < 3; ++j) {
-                System.out.print("*");
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                System.out.print("* ");
             }
             System.out.println();
         }
@@ -25,10 +28,20 @@ public class Patterns {
 
     static void pattern2() {
         for (int i = 0; i < 5; ++i) {
-            for (int j = 0; j < 5; ++j) {
-                System.out.print("*");
+            for (int j = 0; j <= i; ++j) {
+                System.out.print("* ");
             }
             System.out.println();
         }
     }
+
+    static void pattern3() {
+        for (int i = 1; i <= 5; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
 }
