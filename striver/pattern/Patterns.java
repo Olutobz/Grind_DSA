00d49ae7ad420ba1746661958f6a90d614e137b2
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-06-02 01:44:03 +0100

    add pattern number 4 to 6

--- striver/pattern/Patterns.java
+++ striver/pattern/Patterns.java
@@ -9,11 +9,17 @@ public class Patterns {
 
     public static void main(String[] args) {
         pattern1();
-        System.out.println("______________________________________");
+        System.out.println("___________________________");
         pattern2();
-        System.out.println("______________________________________");
+        System.out.println("___________________________");
         pattern3();
-        System.out.println("______________________________________");
+        System.out.println("___________________________");
+        pattern4();
+        System.out.println("___________________________");
+        pattern5();
+        System.out.println("___________________________");
+        pattern6();
+        System.out.println("___________________________");
 
     }
 
@@ -37,6 +43,33 @@ public class Patterns {
 
     static void pattern3() {
         for (int i = 1; i <= 5; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern4() {
+        for (int i = 1; i <= 5; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern5() {
+        for (int i = 0; i < 5; ++i) {
+            for (int j = 5; j > i; --j) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern6() {
+        for (int i = 5; i >= 1; --i) {
             for (int j = 1; j <= i; ++j) {
                 System.out.print(j + " ");
             }
