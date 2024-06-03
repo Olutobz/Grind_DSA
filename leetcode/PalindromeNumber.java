f1de829f453c5ecb333defa63b44966ec1eac634
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-06-03 22:32:47 +0100

    Modify PalindromeNumber class

--- leetcode/PalindromeNumber.java
+++ leetcode/PalindromeNumber.java
@@ -1,20 +1,31 @@
 public class PalindromeNumber {
 
-    // TC -> O(LogN), SC -> O(1);
-    public boolean isPalindrome(int x) {
+    // TC -> O(log x), SC -> O(1);
+    private boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        int original = x;
+        int reversed = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+        return original == reversed;
+    }
+
+    // TC -> O(log x), SC -> O(1);
+    private boolean isPalindromeII(int x) {
         if (x < 0 || (x % 10 == 0 && x != 0)) return false;
         int res = 0;
-
         while (x > res) {
             res = res * 10 + x % 10;
             x /= 10;
         }
-
         return x == res || x == res / 10;
     }
 
-    // TC -> O(N), SC -> O(N);
-    public boolean isPalindromeII(int x) {
+    // TC -> O(x), SC -> O(x);
+    private boolean isPalindromeIII(int x) {
         if (x < 0) return false;
         String temp = Integer.toString(x);
         int i = 0, j = temp.length() - 1;
