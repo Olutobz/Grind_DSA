079cebd30098143b56ab644eeef35f4415bd9af9
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-06-05 23:55:28 +0100

    add problem statement for AddDigits class

--- leetcode/AddDigits.java
+++ leetcode/AddDigits.java
@@ -1,6 +1,21 @@
+/**
+ * Input: num = 38
+ * Output: 2
+ * Explanation: The process is
+ * 38 --> 3 + 8 --> 11
+ * 11 --> 1 + 1 --> 2
+ * Since 2 has only one digit, return it.
+ */
+
 public class AddDigits {
-    // TC -> O(log(num)), SC -> O(1)
+    // TC -> O(1), SC -> O(1)
     public int addDigits(int num) {
+        if (num == 0) return 0;
+        return num % 9 == 0 ? 9 : num % 9;
+    }
+
+    // TC -> O(log(num)), SC -> O(1)
+    public int addDigitsII(int num) {
         if (num == 0) return 0;
         while (num > 9) {
             num = num / 10 + num % 10;
@@ -8,9 +23,4 @@ public class AddDigits {
         return num;
     }
 
-    // TC -> O(1), SC -> O(1)
-    public int addDigitsII(int num) {
-        if (num == 0) return 0;
-        return num % 9 == 0 ? 9 : num % 9;
-    }
 }
