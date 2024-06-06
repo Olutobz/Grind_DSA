8e4f8385a4e90dbe2be02bd9f3050b56ffcebf4c
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-06-06 23:11:17 +0100

    modify ReplaceAllDigitsWithCharacters class

--- leetcode/ReplaceAllDigitsWithCharacters.java
+++ leetcode/ReplaceAllDigitsWithCharacters.java
@@ -1,3 +1,20 @@
+/**
+ * Input: s = "a1c1e1"
+ * Output: "abcdef"
+ * Explanation: The digits are replaced as follows:
+ * - s[1] -> shift('a',1) = 'b'
+ * - s[3] -> shift('c',1) = 'd'
+ * - s[5] -> shift('e',1) = 'f'
+ * *
+ * Input: s = "a1b2c3d4e"
+ * Output: "abbdcfdhe"
+ * Explanation: The digits are replaced as follows:
+ * - s[1] -> shift('a',1) = 'b'
+ * - s[3] -> shift('b',2) = 'd'
+ * - s[5] -> shift('c',3) = 'f'
+ * - s[7] -> shift('d',4) = 'h'
+ */
+
 public class ReplaceAllDigitsWithCharacters {
     // TC -> O(N), SC -> O(N)
     public String replaceDigits(String s) {
