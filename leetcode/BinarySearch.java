70329d28300be9fcd6a38a1f0d976d8755656baf
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-06-09 19:06:33 +0100

    implement round down binary search function

--- leetcode/BinarySearch.java
+++ leetcode/BinarySearch.java
@@ -2,8 +2,11 @@ public class BinarySearch {
 
     // TC -> O(LogN), SC -> O(1)
     public int search(int[] nums, int target) {
-        if (nums == null || nums.length == 0) return -1;
-        int start = 0, end = nums.length - 1;
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int start = 0;
+        int end = nums.length - 1;
         while (start <= end) {
             int mid = start + (end - start) / 2;
             if (nums[mid] == target) {
@@ -15,5 +18,23 @@ public class BinarySearch {
             }
         }
         return -1;
+    }
+
+    // TC -> O(LogN), SC -> O(1)
+    private int searchII(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] >= target) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return nums[start] == target ? start : -1;
     }
 }
