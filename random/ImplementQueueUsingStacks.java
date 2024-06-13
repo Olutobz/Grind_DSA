c32a930c39fb35efacc4151fcf3b31cd857d606f
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-06-13 15:34:02 +0100

    modify ImplementQueueUsingStacks class

--- random/ImplementQueueUsingStacks.java
+++ random/ImplementQueueUsingStacks.java
@@ -1,10 +1,20 @@
 import java.util.Stack;
 
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * <blockquote> <pre>
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ * </pre></blockquote>
+ */
+
 public class ImplementQueueUsingStacks {
     static class MyQueue {
-
-        Stack<Integer> stack;
-        Stack<Integer> tstack;
+        private final Stack<Integer> stack;
+        private final Stack<Integer> tstack;
 
         public MyQueue() {
             stack = new Stack<>();
@@ -12,13 +22,11 @@ public class ImplementQueueUsingStacks {
         }
 
         // TC -> O(1), SC -> O(1)
-        // Pushes element x to the back of the queue.
         public void push(int x) {
             stack.push(x);
         }
 
         // TC -> O(1), SC -> O(1)
-        // Removes the element from in front of queue.
         public int pop() {
             if (tstack.isEmpty()) {
                 while (!stack.isEmpty()) {
@@ -30,7 +38,6 @@ public class ImplementQueueUsingStacks {
         }
 
         // TC -> O(1), SC -> O(1)
-        // Get the front element.
         public int peek() {
             if (tstack.isEmpty()) {
                 while (!stack.isEmpty()) {
@@ -42,18 +49,8 @@ public class ImplementQueueUsingStacks {
         }
 
         // TC -> O(1), SC -> O(1)
-        // Return whether the queue is empty.
         public boolean empty() {
             return stack.isEmpty() && tstack.isEmpty();
         }
     }
-
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue obj = new MyQueue();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.peek();
- * boolean param_4 = obj.empty();
- */
 }
