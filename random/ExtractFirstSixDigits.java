1736b47767cc3622156b5b4eda1d153cc1aeffb8
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-06-13 14:35:30 +0100

    modify ExtractFirstSixDigits class

--- random/ExtractFirstSixDigits.java
+++ random/ExtractFirstSixDigits.java
@@ -6,16 +6,20 @@
 public class ExtractFirstSixDigits {
 
     // TC -> O(1), SC -> O(1)
-    private String getFirstSixDigitsFromString(String numberString) {
-        if (numberString.isEmpty()) return "";
-        return numberString.substring(0, Math.min(6, numberString.length()));
+    private static String getFirstSixDigitsFromString(String numberString) {
+        if (numberString.length() < 6) {
+            throw new IllegalArgumentException("Please enter a string value of size 6");
+        }
+        return numberString.substring(0, 6);
     }
 
-    // TC -> O(1), SC -> O(1) [assuming all the first 6 characters are all digits]
-    private String getFirstSixDigitsFromStringII(String numberString) {
-        if (numberString.isEmpty()) return "";
+    // TC -> O(1), SC -> O(N)
+    private static String getFirstSixDigitsFromStringII(String numberString) {
+        if (numberString.length() < 6) {
+            throw new IllegalArgumentException("Please enter a string value of size 6");
+        }
         StringBuilder res = new StringBuilder();
-        for (int i = 0; i < numberString.length() && i < 6; i++) {
+        for (int i = 0; i < 6; i++) {
             char ch = numberString.charAt(i);
             if (Character.isDigit(ch)) {
                 res.append(ch);
