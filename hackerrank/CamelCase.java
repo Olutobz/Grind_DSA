74e0acf5579858f89dd292b0b7e6a0ef7d78187e
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-06-16 09:56:19 +0100

    fix incorrect space complexity

--- hackerrank/CamelCase.java
+++ hackerrank/CamelCase.java
@@ -1,12 +1,13 @@
 public class CamelCase {
 
-    // TC -> O(N), SC -> O(1)
+    // TC -> O(N), SC -> O(N)
     public static int camelcase(String s) {
         if (s == null || s.isEmpty()) {
             return 0;
         }
         int count = 0;
-        for (char currChar : s.toCharArray()) {
+        char[] charArr = s.toCharArray();
+        for (char currChar : charArr) {
             if (Character.isUpperCase(currChar)) count++;
         }
         return count;
