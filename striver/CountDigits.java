f73fbdb208300c96eabcefabc5778a27052f810e
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-06-19 23:20:09 +0100

    add blockquote comment to CountDigits class

--- striver/CountDigits.java
+++ striver/CountDigits.java
@@ -3,17 +3,33 @@
  * DATE: 02 May 2024
  * EMAIL: damexxey94@gmail.com
  */
+
+/**
+ * <blockquote>
+ * <pre>
+ * Example 1:
+ * Input:N = 12345
+ * Output:5
+ * Explanation:  The number 12345 has 5 digits.
+ *
+ * Example 2:
+ * Input:N = 7789
+ * Output: 4
+ * Explanation: The number 7789 has 4 digits.
+ * </pre>
+ * </blockquote>
+ */
 public class CountDigits {
 
     // TC -> O(1), SC -> O(1)
-    public static int countDigits(Long digits) {
-        return (int) Math.floor(Math.log10(digits) + 1);
+    public static int countDigits(int digits) {
+        return (int) Math.log10(digits) + 1;
     }
 
     // TC -> O(log n), SC -> O(1)
-    public static int countDigitsII(Long digits) {
+    public static int countDigitsII(int digits) {
         int count = 0;
-        while (digits != 0) {
+        while (digits > 0) {
             digits = digits / 10;
             count++;
         }
