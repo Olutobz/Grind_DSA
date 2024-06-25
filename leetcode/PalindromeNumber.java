b9896bc0b5f680fbc7765de69066864cd5be41c8
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-06-25 04:26:40 +0100

    modify PalindromeNumber class

--- leetcode/PalindromeNumber.java
+++ leetcode/PalindromeNumber.java
@@ -13,34 +13,34 @@
 
 public class PalindromeNumber {
 
-    // TC -> O(log x), SC -> O(1);
-    private boolean isPalindrome(int x) {
-        if (x < 0) return false;
-        int original = x;
+    // TC -> O(logN), SC -> O(1);
+    private boolean isPalindrome(int n) {
+        if (n < 0) return false;
+        int original = n;
         int reversed = 0;
-        while (x != 0) {
-            int digit = x % 10;
+        while (n != 0) {
+            int digit = n % 10;
             reversed = reversed * 10 + digit;
-            x /= 10;
+            n /= 10;
         }
         return original == reversed;
     }
 
-    // TC -> O(log x), SC -> O(1);
-    private boolean isPalindromeII(int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+    // TC -> O(logN), SC -> O(1);
+    private boolean isPalindromeII(int n) {
+        if (n < 0 || (n % 10 == 0 && n != 0)) return false;
         int res = 0;
-        while (x > res) {
-            res = res * 10 + x % 10;
-            x /= 10;
+        while (n > res) {
+            res = res * 10 + n % 10;
+            n /= 10;
         }
-        return x == res || x == res / 10;
+        return n == res || n == res / 10;
     }
 
-    // TC -> O(x), SC -> O(x);
-    private boolean isPalindromeIII(int x) {
-        if (x < 0) return false;
-        String temp = Integer.toString(x);
+    // TC -> O(N), SC -> O(N);
+    private boolean isPalindromeIII(int n) {
+        if (n < 0) return false;
+        String temp = Integer.toString(n);
         int i = 0, j = temp.length() - 1;
         while (i < j) {
             if (temp.charAt(++i) != temp.charAt(--j)) {
