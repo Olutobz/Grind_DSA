f5aab19dd607966d114e473f6043d8fa2b50f15c
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-06-26 21:45:31 +0100

    Modify ValidPalindrome class

--- leetcode/ValidPalindrome.java
+++ leetcode/ValidPalindrome.java
@@ -1,37 +1,58 @@
+/**
+ * A phrase is a palindrome if, after converting all uppercase letters into lowercase letters
+ * and removing all non-alphanumeric characters, it reads the same forward and backward.
+ * Alphanumeric characters include letters and numbers.
+ * <p>
+ * <blockquote>
+ * <pre>
+ * Given a string s, return true if it is a palindrome, or false otherwise.
+ *
+ * Example 1:
+ * Input: s = "A man, a plan, a canal: Panama"
+ * Output: true
+ * Explanation: "amanaplanacanalpanama" is a palindrome.
+ *
+ * Example 2:
+ * Input: s = "race a car"
+ * Output: false
+ * Explanation: "raceacar" is not a palindrome.
+ * </pre>
+ * </blockquote>
+ */
+
 public class ValidPalindrome {
 
     // TC -> O(N), SC -> O(1)
     public boolean isPalindrome(String s) {
-        if (s == null || s.isEmpty()) return false;
-        int start = 0, end = s.length() - 1;
-
-        while (start <= end) {
-            char currFirst = s.charAt(start);
-            char currLast = s.charAt(end);
-            if (!Character.isLetterOrDigit(currFirst)) {
+        if (s == null || s.isEmpty()) {
+            return false;
+        }
+        int start = 0;
+        int end = s.length() - 1;
+        while (start < end) {
+            char currStart = s.charAt(start);
+            char currEnd = s.charAt(end);
+            if (!Character.isLetterOrDigit(currStart)) {
                 start++;
-            } else if (!Character.isLetterOrDigit(currLast)) {
+            } else if (!Character.isLetterOrDigit(currEnd)) {
                 end--;
             } else {
-                if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+                if (Character.toLowerCase(currStart) != Character.toLowerCase(currEnd)) {
                     return false;
                 }
                 start++;
                 end--;
             }
-
         }
-
         return true;
     }
 
     // TC -> O(N), SC -> O(N)
     public boolean isPalindromeII(String s) {
-        // remove all the characters that are not A-Z or a-z or 0-9.
         s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
         int start = 0;
         int end = s.length() - 1;
-        while (start <= end) {
+        while (start < end) {
             if (s.charAt(start) != s.charAt(end)) {
                 return false;
             }
