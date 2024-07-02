6586d4a11703b82d3aa527342a414d194306b32e
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-07-02 08:24:51 +0100

    add optimized function to print all divisors of a number

--- striver/basicMath/PrintAllDivisors.java
+++ striver/basicMath/PrintAllDivisors.java
@@ -6,6 +6,9 @@ package basicMath;
  * EMAIL: damexxey94@gmail.com
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Given an integer N, return all divisors of N.
  * <p>
@@ -21,7 +24,7 @@ package basicMath;
  * Explanation: The divisors of 36 are 1, 2, 3, 4, 6, 9, 12, 18, 36.
  *
  * Example 2:
- * Input:N =12
+ * Input:N = 12
  * Output: [1, 2, 3, 4, 6, 12]
  * Explanation: The divisors of 12 are 1, 2, 3, 4, 6, 12.
  * </pre>
@@ -42,15 +45,39 @@ public class PrintAllDivisors {
         return divisors;
     }
 
+    // TC -> O(sqrt(N)), SC -> O(sqrt(N))
+    private static List<Integer> getAllDivisorsII(int n) {
+        List<Integer> divisors = new ArrayList<>();
+
+        for (int i = 1; i <= n / i; i++) {
+            if (n % i == 0) {
+                divisors.add(i);
+
+                if (i != n / i) {
+                    divisors.add(n / i);
+                }
+            }
+        }
+        return divisors;
+    }
+
     private static void printArray(int[] arr) {
         for (int data : arr) {
             if (data != 0) {
                 System.out.print(data + " ");
             }
         }
+        System.out.println();
+    }
+
+    private static void printList(List<Integer> list) {
+        for (int data : list) {
+            System.out.print(data + " ");
+        }
     }
 
     public static void main(String[] args) {
         printArray(getAllDivisors(36));
+        printList(getAllDivisorsII(36));
     }
 }
