523ce394f4571d808aca929d561eb634a155df08
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-07-08 18:16:42 +0100

    modify getInnerMostString function

--- random/bloomberg/FindTheStringsInTheInnermostLevel.java
+++ random/bloomberg/FindTheStringsInTheInnermostLevel.java
@@ -7,48 +7,33 @@ public class FindTheStringsInTheInnermostLevel {
 
     public static void main(String[] args) {
         String str = "abc(d(e)f)ghi[jkl]mno";
-        System.out.println(getString(str));
+        System.out.println(getInnerMostString(str));
     }
 
     // TC -> O(N), SC -> O(N)
-    public static List<String> getString(String str) {
-        List<String> res = new ArrayList<>();
-        if (str == null || str.isEmpty()) return res;
+    public static List<String> getInnerMostString(String str) {
+        if (str == null || str.isEmpty()) return new ArrayList<>();
 
+        List<String> res = new ArrayList<>();
+        StringBuilder temp = new StringBuilder();
+        int max = 0;
         int count = 0;
-        int max = countNestedBrackets(str);
+
         for (char ch : str.toCharArray()) {
             if (ch == '(' || ch == '{' || ch == '[') {
                 count++;
-                str = "";
+                max = Math.max(max, count);
+                temp.setLength(0);
             } else if (ch == ')' || ch == '}' || ch == ']') {
                 if (count == max) {
-                    res.add(str);
+                    res.add(temp.toString());
                 }
                 count--;
             } else {
-                str += ch;
+                temp.append(ch);
             }
-
         }
-
         return res;
-    }
-
-    // TC -> O(N), SC -> O(1)
-    private static int countNestedBrackets(String str) {
-        int count = 0;
-        int max = 0;
-        for (char c : str.toCharArray()) {
-            if (c == '(' || c == '{' || c == '[') {
-                count++;
-                max = Math.max(count, max);
-            } else if (c == ')' || c == '}' || c == ']') {
-                count--;
-            }
-        }
-
-        return max;
     }
 
 }
