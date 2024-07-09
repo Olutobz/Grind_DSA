627583ce1aef9a38f90a7255ab660e247a0e36b6
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-07-09 11:03:41 +0100

    implement multiple methods for finding fibonacci number

--- leetcode/FibonacciNumber.java
+++ leetcode/FibonacciNumber.java
@@ -1,54 +1,69 @@
-import java.util.HashMap;
-import java.util.Map;
-
+/**
+ * Given an integer N. Print the Fibonacci series up to the Nth term.
+ * <blockquote>
+ * <pre>
+ * Example 1:
+ * Input: N = 5
+ * Output: 0 1 1 2 3 5
+ * Explanation: 0 1 1 2 3 5 is the fibonacci series up to 5th term.(0 based indexing)
+ *
+ * Example 2:
+ * Input: 6
+ * Output: 0 1 1 2 3 5 8
+ * Explanation: 0 1 1 2 3 5 8 is the fibonacci series upto 6th term.(0 based indexing)
+ * </pre>
+ * </blockquote>
+ */
 public class FibonacciNumber {
 
-    // TC -> O(N), SC -> O(1)
+    // TC -> O(n), SC -> O(1)
     public int fib(int n) {
         if (n <= 1) return n;
-        int a = 0; // F(n-2)
-        int b = 1; // F(n-1)
-        int sum = 0;
-        while (n > 1) {
-            sum = a + b;
-            a = b;
-            b = sum;
-            n--;
+
+        int last = 0;
+        int secondToLast = 1;
+        int curr = 0;
+        for (int i = 2; i <= n; i++) {
+            curr = last + secondToLast;
+            last = secondToLast;
+            secondToLast = curr;
         }
-        return sum;
+        return curr;
     }
 
-    // TC -> O(N), SC -> O(N)
+    // TC -> O(n), SC -> O(1)
     public int fibII(int n) {
         if (n <= 1) return n;
 
-        int[] map = new int[n + 1];
+        int last = 0;
+        int secondToLast = 1;
+        int curr = 0;
+        while (n > 1) {
+            curr = last + secondToLast;
+            last = secondToLast;
+            secondToLast = curr;
+            n--;
+        }
+        return curr;
+    }
+
+    // TC -> O(n), SC -> O(n)
+    public static int fibIII(int n) {
+        if (n == 0) return 0;
+
+        int[] res = new int[n + 1];
+        res[0] = 0;
+        res[1] = 1;
         for (int i = 2; i <= n; i++) {
-            map[i] = map[i - 1] + map[i - 2];
+            res[i] = res[i - 1] + res[i - 2];
         }
-
-        return map[n];
+        return res[n];
     }
 
-    // TC -> O(2^N), SC -> O(N)
-    public int fibIII(int n) {
+    // TC -> O(2^n), SC -> O(n)
+    public static int fibIV(int n) {
         if (n <= 1) return n;
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(0, 0);
-        map.put(1, 1);
-        return getFibFromMap(n, map);
-
+        return fibIV(n - 1) + fibIV(n - 2);
     }
-
-    // TC -> O(2^n), SC -> O(1)
-    private int getFibFromMap(int n, HashMap<Integer, Integer> map) {
-        if (!map.containsKey(n)) {
-            int currenFib = getFibFromMap(n - 1, map) + getFibFromMap(n - 2, map);
-            map.put(n, currenFib);
-        }
-        return map.get(n);
-    }
-
 
 }
