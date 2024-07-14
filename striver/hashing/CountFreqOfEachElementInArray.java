05c9cfb88a07b3b2338b00f8e3150a247208445b
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-07-14 07:06:34 +0100

    add methods to count freq of each element in an array

---
+++ striver/hashing/CountFreqOfEachElementInArray.java
@@ -0,0 +1,52 @@
+package hashing;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Onikoyi Damola Olutoba
+ * DATE: 13, July 2024
+ * EMAIL: damexxey94@gmail.com
+ */
+
+public class CountFreqOfEachElementInArray {
+
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 10, 15, 10, 5};
+        countFreq(arr);
+        countFreqII(arr);
+    }
+
+    // TC -> O(n^2), SC -> O(n)
+    private static void countFreq(int[] arr) {
+        int len = arr.length;
+        boolean[] visited = new boolean[len];
+        System.out.println("Using boolean array: ");
+        for (int i = 0; i < len; i++) {
+            if (visited[i]) continue;
+            else visited[i] = true;
+            int count = 1;
+            for (int j = i + 1; j < len; j++) {
+                if (arr[i] == arr[j]) {
+                    visited[j] = true;
+                    count++;
+                }
+            }
+            System.out.println(arr[i] + " -> " + count);
+        }
+        System.out.println(Arrays.toString(visited));
+    }
+
+    // TC -> O(n), SC -> O(n)
+    private static void countFreqII(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        System.out.println("Using HashMap: ");
+        for (int item : arr) {
+            map.put(item, map.getOrDefault(item, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+    }
+}
