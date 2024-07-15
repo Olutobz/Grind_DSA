4499c740f75bc52ed6679d0d950f45f4ddab04dc
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-07-15 09:40:21 +0100

    modify problem statement in BinarySearch class

--- leetcode/BinarySearch.java
+++ leetcode/BinarySearch.java
@@ -1,4 +1,8 @@
 /**
+ * Given an array of integers nums which is sorted in ascending order, and an integer target.
+ * write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+ * <p>
+ * You must write an algorithm with O(log n) runtime complexity.
  * <blockquote> <pre>
  * Input: nums = [-1,0,3,5,9,12], target = 9
  * Output: 4
@@ -65,9 +69,9 @@ public class BinarySearch {
         if (nums[mid] == target) {
             return mid;
         } else if (nums[mid] < target) {
-            return recursiveSearch(nums, target, mid + 1, end);
+            return recursiveSearch(nums, target, ++mid, end);
         } else {
-            return recursiveSearch(nums, target, start, mid - 1);
+            return recursiveSearch(nums, target, start, --mid);
         }
     }
 }
