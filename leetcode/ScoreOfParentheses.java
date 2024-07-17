ed3c5eb06853232002935383e9cac38143d73480
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-07-18 00:04:12 +0100

    add ScoreOfParentheses class

---
+++ leetcode/ScoreOfParentheses.java
@@ -0,0 +1,39 @@
+/**
+ * Created by Onikoyi Damola Olutoba
+ * DATE: 14, July 2024
+ * EMAIL: damexxey94@gmail.com
+ */
+
+/**
+ * Given a balanced parentheses string s, return the score of the string.
+ * <p>
+ * The score of a balanced parentheses string is based on the following rule:
+ * <p>
+ * "()" has score 1.
+ * AB has score A + B, where A and B are balanced parentheses strings.
+ * (A) has score 2 * A, where A is a balanced parentheses string.
+ *
+ * <blockquote>
+ * <pre>
+ * Example 1:
+ * Input: s = "()"
+ * Output: 1
+ *
+ * Example 2:
+ * Input: s = "(())"
+ * Output: 2
+ *
+ * Example 3:
+ * Input: s = "()()"
+ * Output: 2
+ * </pre>
+ * </blockquote>
+ */
+
+public class ScoreOfParentheses {
+
+    public int scoreOfParentheses(String s) {
+        return 0;
+    }
+
+}
