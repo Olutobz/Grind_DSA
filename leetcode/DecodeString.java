5e61bd3aace618b3a1778fa41de9767cef5607e5
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-08-07 17:45:26 +0100

    chore: add problem statement to DecodeString

--- leetcode/DecodeString.java
+++ leetcode/DecodeString.java
@@ -1,6 +1,37 @@
 import java.util.Stack;
 
+/**
+ * Given an encoded string, return its decoded string.
+ * <p>
+ * The encoding rule is: k[encoded_string],
+ * where the encoded_string inside the square brackets is being repeated exactly k times.
+ * Note that k is guaranteed to be a positive integer.
+ * <p>
+ * You may assume that the input string is always valid;
+ * there are no extra white spaces, square brackets are well-formed, etc. Furthermore,
+ * you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k.
+ * For example, there will not be input like 3a or 2[4].
+ * <p>
+ * The test cases are generated so that the length of the output will never exceed 105.
+ *
+ * <blockquote>
+ * <pre>
+ * Example 1:
+ * Input: s = "3[a]2[bc]"
+ * Output: "aaabcbc"
+ *
+ * Example 2:
+ * Input: s = "3[a2[c]]"
+ * Output: "accaccacc"
+ *
+ * Example 3:
+ * Input: s = "2[abc]3[cd]ef"
+ * Output: "abcabccdcdcdef"
+ * </pre>
+ * </blockquote>
+ */
 public class DecodeString {
+
     // TC -> O(N), SC -> O(N)
     public String decodeString(String s) {
         if (s == null || s.isEmpty()) return "";
@@ -31,6 +62,6 @@ public class DecodeString {
         }
 
         return res.toString();
-
     }
+
 }
