f9fc14a096587aa734fbde862a4bf3bbbb727e4c
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-09-10 11:59:34 +0100

    move PowerOfTwo class to random package

--- random/PowerOfTwo.java
+++ random/PowerOfTwo.java
@@ -1,6 +1,4 @@
-package bloomberg;
-
-/*
+/**
  * Created by Onikoyi Damola Olutoba
  * DATE: 03 December 2023
  * EMAIL: damexxey94@gmail.com
@@ -12,12 +10,12 @@ package bloomberg;
  * <pre>
  * Let's take some good examples :
  * number->binary form
- * 3->0000 0011
- * 4->0000 0100
- * 5->0000 0101
- * 6->0000 0110
- * 7->0000 0111
- * 8->0000 1000
+ * 3-> 0000 0011
+ * 4-> 0000 0100
+ * 5-> 0000 0101
+ * 6-> 0000 0110
+ * 7-> 0000 0111
+ * 8-> 0000 1000
  * </pre>
  * </blockquote>
  * <p>
