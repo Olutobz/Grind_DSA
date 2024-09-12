03df42355e3e48b8814c9ac38c1790a6a6981ae6
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-09-12 02:48:25 +0100

    update PrintAllDivisors class

--- striver/basic_math/PrintAllDivisors.java
+++ striver/basic_math/PrintAllDivisors.java
@@ -1,54 +1,55 @@
 package basic_math;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Onikoyi Damola Olutoba
  * DATE: 27, June 2024
  * EMAIL: damexxey94@gmail.com
  */
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
- * Given an integer N, return all divisors of N.
+ * Given an integer n, return all divisors of n.
  * <p>
- * A divisor of an integer N is a positive integer that divides N without leaving a remainder.
- * In other words, if N is divisible by another integer without any remainder,
- * then that integer is considered a divisor of N.
+ * A divisor of an integer n is a positive integer that divides n without leaving a remainder.
+ * In other words, if n is divisible by another integer without any remainder,
+ * then that integer is considered a divisor of n.
  *
  * <blockquote>
  * <pre>
  * Example 1:
- * Input:N = 36
+ * Input: n = 36
  * Output:[1, 2, 3, 4, 6, 9, 12, 18, 36]
  * Explanation: The divisors of 36 are 1, 2, 3, 4, 6, 9, 12, 18, 36.
  *
  * Example 2:
- * Input:N = 12
+ * Input: n = 12
  * Output: [1, 2, 3, 4, 6, 12]
  * Explanation: The divisors of 12 are 1, 2, 3, 4, 6, 12.
  * </pre>
  * </blockquote>
  */
+
 public class PrintAllDivisors {
 
-    // TC -> O(N), SC -> O(N)
+    // TC -> O(N), SC -> O(1) [excluding output array]
     private static int[] getAllDivisors(int n) {
         int[] divisors = new int[n];
-        int count = 0;
+        int index = 0;
         for (int i = 1; i <= n; i++) {
             if (n % i == 0) {
-                divisors[count] = i;
+                divisors[index] = i;
             }
-            count++;
+            index++;
         }
+
         return divisors;
     }
 
-    // TC -> O(sqrt(N)), SC -> O(sqrt(N))
+    // TC -> O(sqrt(N)), SC -> O(1) [excluding output array]
     private static List<Integer> getAllDivisorsII(int n) {
         List<Integer> divisors = new ArrayList<>();
-
         for (int i = 1; i <= n / i; i++) {
             if (n % i == 0) {
                 divisors.add(i);
@@ -58,26 +59,31 @@ public class PrintAllDivisors {
                 }
             }
         }
+
         return divisors;
     }
 
     private static void printArray(int[] arr) {
-        for (int data : arr) {
-            if (data != 0) {
-                System.out.print(data + " ");
+        for (int num : arr) {
+            if (num != 0) {
+                System.out.print(num + " ");
             }
         }
         System.out.println();
     }
 
     private static void printList(List<Integer> list) {
-        for (int data : list) {
-            System.out.print(data + " ");
+        for (int number : list) {
+            System.out.print(number + " ");
         }
+        System.out.println();
     }
 
     public static void main(String[] args) {
         printArray(getAllDivisors(36));
+        printArray(getAllDivisors(12));
+        System.out.println("-------------------");
         printList(getAllDivisorsII(36));
+        printList(getAllDivisorsII(12));
     }
 }
