9ac7bd7fe0dcdebe8b58bdd78aed800c704d26b4
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-10-25 22:56:53 +0100

    modify LongestSubstringWithoutRepeatingCharacters

--- leetcode/LongestSubstringWithoutRepeatingCharacters.java
+++ leetcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,12 +1,58 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+
+
+/**
+ * Given a string s, find the length of the longest substring without repeating characters.
+ *
+ * <blockquote>
+ * <pre>
+ * Example 1:
+ * Input: s = "abcabcbb"
+ * Output: 3
+ * Explanation: The answer is "abc", with the length of 3.
+ *
+ * Example 2:
+ * Input: s = "bbbbb"
+ * Output: 1
+ * Explanation: The answer is "b", with the length of 1.
+ *
+ * Example 3:
+ * Input: s = "pwwkew"
+ * Output: 3
+ * Explanation: The answer is "wke", with the length of 3.
+ * Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ *
+ *
+ * Constraints:
+ * 0 <= s.length <= 5 * 104
+ * s consists of English letters, digits, symbols and spaces.
+ * </pre>
+ * </blockquote>
+ */
 
 public class LongestSubstringWithoutRepeatingCharacters {
 
     // TC -> O(N), SC -> O(N)
+    public int lengthOfLongestSubstringII(String s) {
+        if (s == null || s.isEmpty()) return 0;
+
+        Map<Character, Integer> map = new HashMap<>();
+        int max = Integer.MIN_VALUE;
+
+        for (int left = 0, right = 0; right < s.length(); right++) {
+            if (map.containsKey(s.charAt(right))) {
+                left = Math.max(left, map.get(s.charAt(right)) + 1);
+            }
+            map.put(s.charAt(right), right);
+            max = Math.max(max, right - left + 1);
+        }
+        return max;
+    }
+
+    // TC -> O(N), SC -> O(N)
     public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.isEmpty()) return 0;
+
         Set<Character> set = new HashSet<>();
         int left = 0, right = 0, max = 0;
         while (right < s.length()) {
@@ -22,17 +68,18 @@ public class LongestSubstringWithoutRepeatingCharacters {
     }
 
     // TC -> O(N), SC -> O(N)
-    public int lengthOfLongestSubstringII(String s) {
-        if (s.length() == 0) return 0;
-        Map<Character, Integer> map = new HashMap<>();
-        int max = Integer.MIN_VALUE;
+    public int lengthOfLongestSubstringIII(String s) {
+        if (s == null || s.isEmpty()) return 0;
 
-        for (int i = 0, j = 0; i < s.length(); i++) {
-            if (map.containsKey(s.charAt(i))) {
-                j = Math.max(j, map.get(s.charAt(i)) + 1);
+        Queue<Character> queue = new LinkedList<>();
+        int max = 0;
+
+        for (char ch : s.toCharArray()) {
+            while (queue.contains(ch)) {
+                queue.poll();
             }
-            map.put(s.charAt(i), i);
-            max = Math.max(max, i - j + 1);
+            queue.offer(ch);
+            max = Math.max(max, queue.size());
         }
         return max;
     }
