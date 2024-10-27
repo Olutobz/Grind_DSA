ca96b9d3ca8a064ac40adab7481b2db40a73aa3e
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-10-27 11:25:47 +0100

    add problem statement to LongestRepeatingCharacterReplacement class

--- leetcode/LongestRepeatingCharacterReplacement.java
+++ leetcode/LongestRepeatingCharacterReplacement.java
@@ -1,6 +1,36 @@
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * You are given a string s and an integer k. You can choose any character of the string
+ * and change it to any other uppercase English character. You can perform this operation at most k times.
+ * <p>
+ * Return the length of the longest substring containing the same letter
+ * you can get after performing the above operations.
+ *
+ * <blockquote>
+ * <pre>
+ * Example 1:
+ * Input: s = "ABAB", k = 2
+ * Output: 4
+ * Explanation: Replace the two 'A's with two 'B's or vice versa.
+ *
+ * Example 2:
+ * Input: s = "AABABBA", k = 1
+ * Output: 4
+ * Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+ * The substring "BBBB" has the longest repeating letters, which is 4.
+ * There may exists other ways to achieve this answer too.
+ *
+ *
+ * Constraints:
+ * 1 <= s.length <= 105
+ * s consists of only uppercase English letters.
+ * 0 <= k <= s.length
+ * </pre>
+ * </blockquote>
+ */
+
 public class LongestRepeatingCharacterReplacement {
 
     // TC -> O(N), SC -> O(26)
@@ -36,9 +66,9 @@ public class LongestRepeatingCharacterReplacement {
         int maxFreq = 0;
 
         for (int right = 0; right < s.length(); right++) {
-            char currCh = s.charAt(right);
-            map.put(currCh, map.getOrDefault(currCh, 0) + 1);
-            maxFreq = Math.max(maxFreq, map.get(currCh));
+            char ch = s.charAt(right);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            maxFreq = Math.max(maxFreq, map.get(ch));
             int lettersToChange = (right - left + 1) - maxFreq;
 
             if (lettersToChange > k) {
