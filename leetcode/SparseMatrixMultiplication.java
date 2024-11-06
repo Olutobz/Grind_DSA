11fd0bb22b557d285b9d37cee2954b99252912d4
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-11-06 09:56:24 +0100

    add extra implementation for SparseMatrixMultiplication

--- leetcode/SparseMatrixMultiplication.java
+++ leetcode/SparseMatrixMultiplication.java
@@ -1,52 +1,98 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.ArrayList;
+import java.util.*;
 
+/**
+ * Given two sparse matrices A of size m x k and B of size k x n, return the result of AB.
+ * You may assume that A's column number is equal to B's row number; also that multiplication is always possible.
+ * <blockquote>
+ * <pre>
+ * Example 1:
+ * Input: A = [[1,0,0],[-1,0,3]], B = [[7,0,0],[0,0,0],[0,0,1]]
+ * Output: [[7,0,0],[-7,0,3]]
+ *
+ * Example 2:
+ * Input: A = [[0]], B = [[0]]
+ * Output: [[0]]
+ * </pre>
+ * </blockquote>
+ */
 public class SparseMatrixMultiplication {
 
-    /**
-     * Given two sparse matrices mat1 of size m x k and mat2 of size k x n, return the result of mat1 x mat2.
-     * You may assume that multiplication is always possible.
-     * <p>
-     * Example 1:
-     * Input: mat1 = [[1,0,0],[-1,0,3]], mat2 = [[7,0,0],[0,0,0],[0,0,1]]
-     * Output: [[7,0,0],[-7,0,3]]
-     * <p>
-     * Example 2:
-     * Input: mat1 = [[0]], mat2 = [[0]]
-     * Output: [[0]]
-     */
+    // TC -> O(m * n * k), SC -> O(m * n), where m = A and n = B
+    public static int[][] multiply(int[][] A, int[][] B) {
+        if (A == null || B == null || A.length == 0 || B.length == 0) {
+            return new int[0][0];
+        }
 
-    // TC -> O(N * M), SC -> O(max(N, M)), where M = mat1 and N = mat2
-    public int[][] multiply(int[][] mat1, int[][] mat2) {
-        int r1 = mat1.length;
-        int c1 = mat1[0].length;
-        int c2 = mat2[0].length;
+        int aRow = A.length;
+        int aCol = A[0].length;
+        int bCol = B[0].length;
 
-        int[][] res = new int[r1][c2];
-
-        Map<Integer, List<Integer>> map = new HashMap<>();
-
-        for (int i = 0; i < r1; ++i) {
-            for (int j = 0; j < c1; ++j) {
-                if (mat1[i][j] != 0) {
-                    map.computeIfAbsent(i, k -> new ArrayList<>()).add(j);
+        Map<Integer, Map<Integer, Integer>> nonZeroMap = new HashMap<>();
+        for (int i = 0; i < aRow; i++) {
+            nonZeroMap.put(i, new HashMap<>());
+            for (int j = 0; j < aCol; j++) {
+                if (A[i][j] != 0) {
+                    nonZeroMap.get(i).put(j, A[i][j]);
                 }
             }
         }
 
-        for (int i = 0; i < r1; ++i) {
-            for (int j = 0; j < c2; ++j) {
-                if (map.containsKey(i)) {
-                    for (int k : map.get(i)) {
-                        res[i][j] += mat1[i][k] * mat2[k][j];
+        int[][] result = new int[aRow][bCol];
+        for (int i = 0; i < aRow; i++) {
+            for (int k : nonZeroMap.get(i).keySet()) {
+                for (int j = 0; j < bCol; j++) {
+                    if (B[k][j] != 0) {
+                        result[i][j] += nonZeroMap.get(i).get(k) * B[k][j];
                     }
                 }
             }
         }
 
-        return res;
+        return result;
+    }
+
+    // TC -> O(m * n), SC -> O(max(n, m)), where m = A and n = B
+    public static int[][] multiplyII(int[][] A, int[][] B) {
+        if (A == null || B == null || A.length == 0 || B.length == 0) {
+            return new int[0][0];
+        }
+        int aRow = A.length;
+        int aCol = A[0].length;
+        int bCol = B[0].length;
+
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < aRow; ++i) {
+            for (int j = 0; j < aCol; ++j) {
+                if (A[i][j] != 0) {
+                    map.computeIfAbsent(i, k -> new ArrayList<>()).add(j);
+                }
+            }
+        }
+
+        int[][] result = new int[aRow][bCol];
+        for (int i = 0; i < aRow; ++i) {
+            for (int j = 0; j < bCol; ++j) {
+                if (map.containsKey(i)) {
+                    for (int k : map.get(i)) {
+                        result[i][j] += A[i][k] * B[k][j];
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(multiply(
+                new int[][]{{1, 0, 0}, {-1, 0, 3}},
+                new int[][]{{7, 0, 0}, {0, 0, 0}, {0, 0, 1}}
+        )));
+
+        System.out.println(Arrays.deepToString(multiplyII(
+                new int[][]{{1, 0, 0}, {-1, 0, 3}},
+                new int[][]{{7, 0, 0}, {0, 0, 0}, {0, 0, 1}}
+        )));
     }
 
 }
