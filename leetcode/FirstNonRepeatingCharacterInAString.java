385dc22cd0f34e68e5ca465a37fadbfe0c8b77b4
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-11-09 12:30:05 +0100

    modify FirstNonRepeatingCharacterInAString

--- leetcode/FirstNonRepeatingCharacterInAString.java
+++ leetcode/FirstNonRepeatingCharacterInAString.java
@@ -1,26 +1,48 @@
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * A string was given. I needed to find the first non-repeating counter.
+ * If there is no such character, return 0.
+ *
+ * <blockquote>
+ * <pre>
+ * Input: s= "asubsebusapd"
+ * Output: e
+ *
+ * Input: s = "aabbcc"
+ * Output: 0
+ * </pre>
+ * </blockquote>
+ */
+
 public class FirstNonRepeatingCharacterInAString {
 
-    // TC -> O(N), SC -> O(26);
+    // TC -> O(N), SC -> O(1);
     public char findFirstUniqueChar(String s) {
-        if (s == null || s.length() == 0) return ' ';
+        if (s == null || s.isEmpty()) {
+            return ' ';
+        }
+
         int[] freq = new int[26];
         for (int i = 0; i < s.length(); i++) {
             freq[s.charAt(i) - 'a']++;
         }
+
         for (int i = 0; i < s.length(); i++) {
             if (freq[(s.charAt(i) - 'a')] == 1) {
                 return s.charAt(i);
             }
         }
+
         return ' ';
     }
 
     // TC -> O(N), SC -> O(N);
     public char firstUniqChar(String s) {
-        if (s == null || s.length() == 0) return ' ';
+        if (s == null || s.isEmpty()) {
+            return ' ';
+        }
 
         Map<Character, Integer> map = new HashMap<>();
         for (char ch : s.toCharArray()) {
@@ -34,8 +56,6 @@ public class FirstNonRepeatingCharacterInAString {
         }
 
         return ' ';
-
     }
-
 
 }
