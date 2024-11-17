beee0fa04bbce183719e0328d3df435c7a41f8c0
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-11-17 23:58:29 +0100

    modify LongestPalindromicSubstring

--- leetcode/LongestPalindromicSubstring.java
+++ leetcode/LongestPalindromicSubstring.java
@@ -1,21 +1,45 @@
+/**
+ * Given a string s, return the longest
+ * palindromic
+ * substring in s.
+ *
+ * <blockquote>
+ * <pre>
+ * Example 1:
+ * Input: s = "babad"
+ * Output: "bab"
+ * Explanation: "aba" is also a valid answer.
+ *
+ * Example 2:
+ * Input: s = "cbbd"
+ * Output: "bb"
+ *
+ *
+ * Constraints:
+ * 1 <= s.length <= 1000
+ * s consist of only digits and English letters.
+ * </pre>
+ * </blockquote>
+ */
+
 public class LongestPalindromicSubstring {
     // TC -> O(N^2), SC -> O(1)
     public String longestPalindrome(String s) {
         String res = "";
         int currLen = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (isPalindrome(s, i - currLen - 1, i)) {
-                res = s.substring(i - currLen - 1, i + 1);
+            int oddLen = i - currLen - 1;
+            int evenLen = i - currLen;
+            if (isPalindrome(s, oddLen, i)) {
+                res = s.substring(oddLen, i + 1);
                 currLen += 2;
-
-            } else if (isPalindrome(s, i - currLen, i)) {
-                res = s.substring(i - currLen, i + 1);
+            } else if (isPalindrome(s, evenLen, i)) {
+                res = s.substring(evenLen, i + 1);
                 currLen++;
             }
         }
 
         return res;
-
     }
 
     // TC -> O(N), SC -> O(1)
