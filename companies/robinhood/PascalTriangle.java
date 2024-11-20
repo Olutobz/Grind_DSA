21eec22bb877679464b2f3d1dfa3d446886607df
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-11-20 23:59:43 +0100

    moved pascal triangles

--- companies/robinhood/PascalTriangle.java
+++ companies/robinhood/PascalTriangle.java
@@ -1,3 +1,5 @@
+package robinhood;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,10 +12,10 @@ public class PascalTriangle {
         for (int i = 0; i < numRows; i++) {
             List<Integer> list = new ArrayList<>();
             for (int j = 0; j <= i; j++) {
-                if (j == 0 || j == i) list.add(1);
-                else {
-                    list.add(pascal.get(i - 1).get(j)
-                            + pascal.get(i - 1).get(j - 1));
+                if (j == 0 || j == i) {
+                    list.add(1);
+                } else {
+                    list.add(pascal.get(i - 1).get(j) + pascal.get(i - 1).get(j - 1));
                 }
             }
             pascal.add(list);
--- companies/robinhood/PascalTriangleII.java
+++ companies/robinhood/PascalTriangleII.java
@@ -1,3 +1,5 @@
+package robinhood;
+
 import java.util.ArrayList;
 import java.util.List;
 
