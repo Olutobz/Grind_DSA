ab771477db83c4fd07ac4f9abcdad88fb6d92476
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-11-21 11:10:02 +0100

    modify BestTimeToBuyAndSellStock

--- companies/robinhood/BestTimeToBuyAndSellStock.java
+++ companies/robinhood/BestTimeToBuyAndSellStock.java
@@ -29,26 +29,38 @@ public class BestTimeToBuyAndSellStock {
 
     // TC -> O(N), SC -> O(1)
     public int maxProfit(int[] prices) {
-        if (prices == null || prices.length == 0) return 0;
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+
         int buy = Integer.MAX_VALUE;
         int sell = 0;
+
         for (int price : prices) {
             buy = Math.min(buy, price);
             sell = Math.max(sell, price - buy);
         }
+
         return sell;
     }
 
     // TC -> O(N), SC -> O(1)
     public int maxProfitI(int[] prices) {
-        if (prices == null || prices.length == 0) return 0;
-        int maxProfit = 0, minPrice = Integer.MAX_VALUE;
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+
         for (int price : prices) {
-            if (price < minPrice) minPrice = price;
-            else if (price - minPrice > maxProfit) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > maxProfit) {
                 maxProfit = price - minPrice;
             }
         }
+
         return maxProfit;
     }
 }
