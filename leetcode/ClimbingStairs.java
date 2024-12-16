e0acb0e9c4973a4809e56d5f6200a36c5b0be680
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-12-17 00:36:36 +0100

    add ClimbingStairs problem

---
+++ leetcode/ClimbingStairs.java
@@ -0,0 +1,32 @@
+/**
+ * Created by Damola Olutoba Onikoyi
+ * DATE: 26, November 2024
+ * EMAIL: damexxey94@gmail.com
+ */
+
+
+public class ClimbingStairs {
+
+    public int climbStairs(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+}
