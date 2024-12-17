2752a337d92a67fb1ac370aee8a3bbaab9eea31e
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-12-18 00:37:15 +0100

    clean up code

--- leetcode/FindPivotIndex.java
+++ leetcode/FindPivotIndex.java
@@ -4,14 +4,18 @@ public class FindPivotIndex {
     public int pivotIndex(int[] nums) {
         if (nums.length == 0) return -1;
         int leftSum = 0, rightSum = 0;
-        for (int num : nums)
+        for (int num : nums) {
             rightSum += num;
+        }
 
         for (int i = 0; i < nums.length; i++) {
             rightSum -= nums[i];
-            if (rightSum == leftSum) return i;
+            if (rightSum == leftSum) {
+                return i;
+            }
             leftSum += nums[i];
         }
+
         return -1;
     }
 
