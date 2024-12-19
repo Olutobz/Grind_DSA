d74c68b846ac8419b27204def7a2663544e42010
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-12-20 00:40:12 +0100

    add samples to problem statement for PivotIndex

--- leetcode/FindPivotIndex.java
+++ leetcode/FindPivotIndex.java
@@ -6,6 +6,34 @@
  * If the index is on the left edge of the array, then the left sum is 0
  * because there are no elements to the left. This also applies to the right edge of the array.
  * Return the leftmost pivot index. If no such index exists, return -1.
+ *
+ * <p>
+ * <blockquote>
+ * <pre>
+ * Example 1:
+ * Input: nums = [1,7,3,6,5,6]
+ * Output: 3
+ * Explanation:
+ * The pivot index is 3.
+ * Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+ * Right sum = nums[4] + nums[5] = 5 + 6 = 11
+ * Example 2:
+ *
+ * Input: nums = [1,2,3]
+ * Output: -1
+ * Explanation:
+ * There is no index that satisfies the conditions in the problem statement.
+ * Example 3:
+ *
+ * Input: nums = [2,1,-1]
+ * Output: 0
+ * Explanation:
+ * The pivot index is 0.
+ * Left sum = 0 (no elements to the left of index 0)
+ * Right sum = nums[1] + nums[2] = 1 + -1 = 0
+ *
+ * </pre>
+ * </blockquote>
  */
 
 public class FindPivotIndex {
