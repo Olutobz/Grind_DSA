86e9ab9536af815a322dae59055527f8f71c2c74
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2024-12-21 00:40:59 +0100

    add constraints to PivotIndex problem statement

--- leetcode/FindPivotIndex.java
+++ leetcode/FindPivotIndex.java
@@ -32,6 +32,10 @@
  * Left sum = 0 (no elements to the left of index 0)
  * Right sum = nums[1] + nums[2] = 1 + -1 = 0
  *
+ *  Constraints:
+ *  1 <= nums.length <= 104
+ *  -1000 <= nums[i] <= 1000
+ *
  * </pre>
  * </blockquote>
  */
