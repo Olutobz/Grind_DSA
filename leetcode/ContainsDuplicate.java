a8056d62ecaa86b9031937ce009531c76d679129
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2025-04-22 16:15:57 +0100

    add problem statement to ContainsDuplicate

--- leetcode/ContainsDuplicate.java
+++ leetcode/ContainsDuplicate.java
@@ -3,6 +3,36 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * Given an integer array nums, return true if any value appears at least twice in the array,
+ * and return false if every element is distinct.
+ *
+ * <blockquote>
+ * <pre>
+ * Example 1:
+ * Input: nums = [1,2,3,1]
+ * Output: true
+ * Explanation:
+ * The element 1 occurs at the indices 0 and 3.
+ *
+ * Example 2:
+ * Input: nums = [1,2,3,4]
+ * Output: false
+ * Explanation:
+ * All elements are distinct.
+ *
+ * Example 3:
+ * Input: nums = [1,1,1,3,3,4,3,2,4,2]
+ * Output: true
+ *
+ * Constraints:
+ * 1 <= nums.length <= 105
+ * -109 <= nums[i] <= 109
+ *
+ * </pre>
+ * </blockquote>
+ */
+
 public class ContainsDuplicate {
 
     // TC -> O(N), SC -> O(N)
