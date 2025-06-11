0bdbaaf355a700fcae1274b6ecfc70f17c651812
Damola Olutoba Onikoyi <damexxey94@gmail.com>
2025-06-11 21:40:44 +0100

    fix problem statement

--- leetcode/java/TwoSum.java
+++ leetcode/java/TwoSum.java
@@ -24,9 +24,9 @@ import java.util.Map;
  * Output: [0,1]
  *
  * Constraints:
- * 2 <= nums.length <= 104
- * -109 <= nums[i] <= 109
- * -109 <= target <= 109
+ * 2 <= nums.length <= 10^4
+ * -10^9 <= nums[i] <= 10^9
+ * -10^9 <= target <= 109
  * Only one valid answer exists.
  * </pre>
  * </blockquote>
