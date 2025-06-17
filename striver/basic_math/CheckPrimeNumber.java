797a6bfd0a9133f6265029bed2f4a0a4c66e46e3
Damola Olutoba Onikoyi <damexxey94@gmail.com>
2025-06-17 22:40:33 +0100

    adjust problem statement

--- striver/basic_math/CheckPrimeNumber.java
+++ striver/basic_math/CheckPrimeNumber.java
@@ -8,7 +8,7 @@ package basic_math;
 
 /**
  * Given an integer N, check whether it is prime or not.
- * A prime number is a number that is only divisible by 1 and itself and the total number of divisors is 2.
+ * A prime number is a number that is only divisible by 1 and itself, and the total number of divisors is 2.
  * <blockquote>
  * <pre>
  * Example 1:
