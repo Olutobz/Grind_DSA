35d86d323c5d37db17337479c4a5e4e1a59fb2bf
Damola Olutoba Onikoyi <damexxey94@gmail.com>
2025-06-20 22:41:16 +0100

    add missing "the" in the problem statement

--- companies/robinhood/PrefixPalindrome.java
+++ companies/robinhood/PrefixPalindrome.java
@@ -10,7 +10,7 @@ package robinhood;
  * Constraint: 0<=len(s)<=1000
  * Given a string:
  * 1. Take all prefixes of the string and choose the longest palindrome between them.
- * 2. If chosen prefix has at least two characters,
+ * 2. If the chosen prefix has at least two characters,
  * cut this from s and go back to step 1 with the updated string. Otherwise,
  * end the algo with the current string s.
  */
