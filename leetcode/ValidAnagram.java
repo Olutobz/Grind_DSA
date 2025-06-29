f15d92562959538850d5b323d79aec7782874bce
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2025-06-29 18:28:57 +0100

    Add problem statement and more solutions to ValidAnagram

--- leetcode/ValidAnagram.java
+++ leetcode/ValidAnagram.java
@@ -2,13 +2,40 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+ *
+ * <blockquote>
+ * <pre>
+ * Example 1:
+ * Input: s = "anagram", t = "nagaram"
+ * Output: true
+ *
+ * Example 2:
+ * Input: s = "rat", t = "car"
+ * Output: false
+ *
+ * Constraints:
+ * 1 <= s.length, t.length <= 5 * 104
+ * s and t consist of lowercase English letters.
+ *
+ * Follow up: What if the inputs contain Unicode characters?
+ * How would you adapt your solution to such a case?
+ * </pre>
+ * </blockquote>
+ */
+
 public class ValidAnagram {
 
     // TC -> O(NLogN), SC -> O(N)
     public boolean isAnagram(String s, String t) {
-        if (s == null && t == null) return true;
-        else if (s == null || t == null) return false;
-        else if (s.length() != t.length()) return false;
+        if (s == null && t == null) {
+            return true;
+        } else if (s == null || t == null) {
+            return false;
+        } else if (s.length() != t.length()) {
+            return false;
+        }
 
         char[] s1 = s.toCharArray();
         char[] s2 = t.toCharArray();
@@ -19,9 +46,34 @@ public class ValidAnagram {
 
     // TC -> O(N), SC -> O(N)
     public boolean isAnagramII(String s, String t) {
-        if (s == null && t == null) return true;
-        else if (s == null || t == null) return false;
-        else if (s.length() != t.length()) return false;
+        if (s == null && t == null) {
+            return true;
+        } else if (s == null || t == null) {
+            return false;
+        } else if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> sMap = new HashMap<>();
+        Map<Character, Integer> tMap = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i), 0) + 1);
+            tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i), 0) + 1);
+        }
+
+        return sMap.equals(tMap);
+    }
+
+    // TC -> O(N), SC -> O(N)
+    public boolean isAnagramIII(String s, String t) {
+        if (s == null && t == null) {
+            return true;
+        } else if (s == null || t == null) {
+            return false;
+        } else if (s.length() != t.length()) {
+            return false;
+        }
 
         Map<Character, Integer> map = new HashMap<>();
 
@@ -30,31 +82,39 @@ public class ValidAnagram {
             map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) - 1);
         }
 
-        for (char c : map.keySet()) {
-            if (map.get(c) != 0) return false;
+        for (char currKey : map.keySet()) {
+            if (map.get(currKey) != 0) {
+                return false;
+            }
         }
 
         return true;
     }
 
-    // TC -> O(N), SC -> O(1) (since we are using an array of constant size of 26 & lower chars)
-    public boolean isAnagramIII(String s, String t) {
-        if (s == null && t == null) return true;
-        else if (s == null || t == null) return false;
-        else if (s.length() != t.length()) return false;
+    // TC -> O(N), SC -> O(26) (since we are using an array of constant size 26 and lowercase chars)
+    public boolean isAnagramIV(String s, String t) {
+        if (s == null && t == null) {
+            return true;
+        } else if (s == null || t == null) {
+            return false;
+        } else if (s.length() != t.length()) {
+            return false;
+        }
 
         int[] freq = new int[26];
 
-        for (int c : s.toCharArray()) {
-            freq[c - 'a']++;
+        for (int ch : s.toCharArray()) {
+            freq[ch - 'a']++;
         }
 
-        for (char c : t.toCharArray()) {
-            freq[c - 'a']--;
+        for (char ch : t.toCharArray()) {
+            freq[ch - 'a']--;
         }
 
         for (int count : freq) {
-            if (count != 0) return false;
+            if (count != 0) {
+                return false;
+            }
         }
 
         return true;
