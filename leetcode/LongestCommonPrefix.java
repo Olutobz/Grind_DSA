c303f19af79d5e84e702d5c02a868c067c6eef08
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2025-07-02 22:51:07 +0100

    modified LongestCommonPrefix

--- leetcode/LongestCommonPrefix.java
+++ leetcode/LongestCommonPrefix.java
@@ -1,25 +1,34 @@
 import java.util.Arrays;
 
+/**
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ * If there is no common prefix, return an empty string "".
+ *
+ * <blockquote>
+ * <pre>
+ * Example 1:
+ * Input: strs = ["flower","flow","flight"]
+ * Output: "fl"
+ *
+ * Example 2:
+ * Input: strs = ["dog","racecar","car"]
+ * Output: ""
+ * Explanation: There is no common prefix among the input strings.
+ *
+ * Constraints:
+ * 1 <= strs.length <= 200
+ * 0 <= strs[i].length <= 200
+ * strs[i] consists of only lowercase English letters if it is non-empty.
+ * </pre>
+ * </blockquote>
+ */
+
 public class LongestCommonPrefix {
 
-    // TC -> O(NLogN), SC -> O(1)
+    // TC -> O(n * m), SC -> O(1)
     public String longestCommonPrefix(String[] str) {
         if (str == null || str.length == 0) return "";
 
-        Arrays.sort(str);
-        String first = str[0];
-        String last = str[str.length - 1];
-        int c = 0;
-        while (c < first.length()) {
-            if (first.charAt(c) == last.charAt(c)) c++;
-            else break;
-        }
-        return c == 0 ? "" : first.substring(0, c);
-    }
-
-    // TC -> O(NM), SC -> O(1)
-    public String longestCommonPrefixII(String[] str) {
-        if (str == null || str.length == 0) return "";
         String prefix = str[0];
         for (int index = 1; index < str.length; index++) {
             while (str[index].indexOf(prefix) != 0) {
@@ -27,6 +36,22 @@ public class LongestCommonPrefix {
             }
         }
         return prefix;
+    }
+
+    // TC -> O(N * MLogM), SC -> O(1)
+    public String longestCommonPrefixII(String[] str) {
+        if (str == null || str.length == 0) return "";
+
+        Arrays.sort(str);
+        String first = str[0];
+        String last = str[str.length - 1];
+        int c = 0;
+
+        while (c < first.length()) {
+            if (first.charAt(c) == last.charAt(c)) c++;
+            else break;
+        }
+        return c == 0 ? "" : first.substring(0, c);
     }
 
 }
