d5652c768efef824999668770ac3a96bdfcbfa90
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2025-07-03 19:31:48 +0100

    fix grammar error for basic calculator problem statement

--- leetcode/BasicCalculator.java
+++ leetcode/BasicCalculator.java
@@ -3,9 +3,9 @@ import java.util.Stack;
 
 /**
  * Given a string s representing a valid expression,
- * implement a basic calculator to evaluate it, and return the result of the evaluation.
+ * implement a basic calculator to evaluate it and return the result of the evaluation.
  * <p>
- * Note: You are not allowed to use any built-in function which evaluates
+ * Note: You are not allowed to use any built-in function that evaluates
  * strings as mathematical expressions, such as eval().
  *
  * <blockquote>
