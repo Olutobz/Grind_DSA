90c55c7560fc896757a79a262d645f357deae37d
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2025-07-04 10:03:12 +0100

    fix sentence error in a problem statement for DailyTemperatures

--- leetcode/DailyTemperatures.java
+++ leetcode/DailyTemperatures.java
@@ -8,7 +8,7 @@
 import java.util.Stack;
 
 /**
- * Given an array of integers temperatures represents the daily temperatures,
+ * Given an array of integers temperatures represent the daily temperatures,
  * return an array answer such that answer[i] is the number of days
  * you have to wait after the ith day to get a warmer temperature.
  * If there is no future day for which this is possible, keep answer[i] == 0 instead.
