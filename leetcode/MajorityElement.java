e4482444884aaeaebed9a99c130e54b9378b7c3e
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2025-07-06 12:55:02 +0100

    add problem statement to MajorityElement

--- leetcode/MajorityElement.java
+++ leetcode/MajorityElement.java
@@ -3,6 +3,32 @@ import java.util.HashMap;
 import java.util.Map;
 
 
+/**
+ * Given an array nums of size n, return the majority element.
+ * The majority element is the element that appears more than ⌊n / 2⌋ times.
+ * You may assume that the majority element always exists in the array.
+ *
+ * <blockquote>
+ * <pre>
+ * Example 1:
+ * Input: nums = [3,2,3]
+ * Output: 3
+ *
+ * Example 2:
+ * Input: nums = [2,2,1,1,1,2,2]
+ * Output: 2
+ *
+ * Constraints:
+ * n == nums.length
+ * 1 <= n <= 5 * 104
+ * -109 <= nums[i] <= 109
+ *
+ * Follow-up: Could you solve the problem in linear time and in O(1) space?
+ * </pre>
+ * </blockquote>
+ */
+
+
 public class MajorityElement {
 
     // TC -> O(n^2), SC -> O(1)
