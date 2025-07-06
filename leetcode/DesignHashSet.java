f9ddb1fd6c756cf70522ee6d3acc60bb53944aac
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2025-07-06 16:00:29 +0100

    solve DesignHashSet problem

--- leetcode/DesignHashSet.java
+++ leetcode/DesignHashSet.java
@@ -5,6 +5,65 @@
  */
 
 
-public class DesignHashSet {
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * Design a HashSet without using any built-in hash table libraries.
+ * Implement MyHashSet class:
+ * void add(key) Inserts the value key into the HashSet.
+ * bool contains(key) Returns whether the value key exists in the HashSet or not.
+ * void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
+ *
+ * <blockquote>
+ * <pre>
+ * Example 1:
+ * Input
+ * ["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
+ * [[], [1], [2], [1], [3], [2], [2], [2], [2]]
+ * Output
+ * [null, null, null, true, false, null, true, null, false]
+ *
+ * Explanation
+ * MyHashSet myHashSet = new MyHashSet();
+ * myHashSet.add(1);      // set = [1]
+ * myHashSet.add(2);      // set = [1, 2]
+ * myHashSet.contains(1); // return True
+ * myHashSet.contains(3); // return False, (not found)
+ * myHashSet.add(2);      // set = [1, 2]
+ * myHashSet.contains(2); // return True
+ * myHashSet.remove(2);   // set = [1]
+ * myHashSet.contains(2); // return False, (already removed)
+ *
+ *
+ * Constraints:
+ *
+ * 0 <= key <= 106
+ * At most 104 calls will be made to add, remove, and contains.
+ * </pre>
+ * </blockquote>
+ */
+
+
+// TC -> O(n), SC -> O(n)
+public class DesignHashSet {
+    private final List<Integer> list;
+
+    public DesignHashSet() {
+        list = new ArrayList<>();
+    }
+
+    public void add(int key) {
+        if (!list.contains(key)) {
+            list.add(key);
+        }
+    }
+
+    public void remove(int key) {
+        list.remove(Integer.valueOf(key));
+    }
+
+    public boolean contains(int key) {
+        return list.contains(key);
+    }
 }
