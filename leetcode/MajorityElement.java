0dbb45c9aac5f7382cbd1bca034f9e4e4c8017b0
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2025-07-06 12:08:28 +0100

    modify boyer-moore algorithm in MajorityElement

--- leetcode/MajorityElement.java
+++ leetcode/MajorityElement.java
@@ -5,24 +5,22 @@ import java.util.Map;
 
 public class MajorityElement {
 
-    // TC -> O(N^2), SC -> O(1)
+    // TC -> O(n^2), SC -> O(1)
     public int majorityElement(int[] nums) {
         if (nums.length == 1) return nums[0];
-
-        int majorElem = nums.length / 2;
 
         for (int num : nums) {
             int count = 0;
             for (int elem : nums) {
                 if (num == elem) count++;
             }
-            if (count > majorElem) return num;
+            if (count > nums.length / 2) return num;
         }
 
         return -1;
     }
 
-    // TC -> O(NLogN), SC -> O(1)
+    // TC -> O(nlogn), SC -> O(1)
     public int majorityElementI(int[] nums) {
         if (nums.length == 1) return nums[0];
 
@@ -31,8 +29,27 @@ public class MajorityElement {
         return nums[n / 2];
     }
 
-    // TC -> O(N), SC -> O(N)
+    // TC -> O(n), SC -> O(n)
     public int majorityElementII(int[] nums) {
+        if (nums.length == 1) return nums[0];
+
+        Map<Integer, Integer> countMap = new HashMap<>();
+        int maxCount = 0;
+        int res = 0;
+
+        for (int num : nums) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+            if (countMap.get(num) > maxCount) {
+                maxCount = countMap.get(num);
+                res = num;
+            }
+        }
+
+        return res;
+    }
+
+    // TC -> O(n), SC -> O(n)
+    public int majorityElementIII(int[] nums) {
         if (nums.length == 1) return nums[0];
 
         int majorElem = nums.length / 2;
@@ -48,15 +65,16 @@ public class MajorityElement {
         return majorElem;
     }
 
-    // TC -> O(N), SC -> O(1)
+    // TC -> O(n), SC -> O(1)
     /* Boyer–Moore majority vote algorithm */
-    public int majorityElementIII(int[] nums) {
+    public int majorityElementIV(int[] nums) {
         if (nums.length == 1) return nums[0];
-        int candidate = 0, count = 0;
+        int candidate = 0;
+        int count = 0;
 
         for (int num : nums) {
             if (count == 0) candidate = num;
-            count += (candidate != num) ? -1 : 1;
+            count += (candidate == num) ? 1 : -1;
         }
 
         return candidate;
