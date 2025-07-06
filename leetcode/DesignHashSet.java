7a97284f7c48095021ad446bf8d9b07de38692dc
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2025-07-06 19:17:00 +0100

    modify DesignHashSet

--- leetcode/DesignHashSet.java
+++ leetcode/DesignHashSet.java
@@ -89,3 +89,11 @@ class DesignHashSetII {
         return list.contains(key);
     }
 }
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
