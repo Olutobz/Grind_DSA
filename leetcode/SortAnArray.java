a0fed70e8a0b07ea9de52ba18353a00acb7fc566
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2025-07-08 09:57:52 +0100

    fix space complexity for in-built merge sort algo

--- leetcode/SortAnArray.java
+++ leetcode/SortAnArray.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class SortAnArray {
 
-    // TC -> O(NLogN), SC -> O(1)
+    // TC -> O(nlogn), SC -> O(n)
     public int[] sortArray(int[] nums) {
         Arrays.sort(nums);
         return nums;
