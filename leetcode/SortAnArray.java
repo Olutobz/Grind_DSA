f5bcfc3d7537334c90a4175da5120aaff545cb4d
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2025-07-11 22:34:03 +0100

    remove in-built sorting from SortAnArray

--- leetcode/SortAnArray.java
+++ leetcode/SortAnArray.java
@@ -1,11 +1,45 @@
-import java.util.Arrays;
-
 public class SortAnArray {
 
     // TC -> O(nlogn), SC -> O(n)
     public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
+        mergeSort(nums);
         return nums;
+    }
+
+    public void mergeSort(int[] arr) {
+        if (arr.length < 2) return;
+
+        int mid = arr.length / 2;
+        int[] leftArr = new int[mid];
+        int[] rightArr = new int[arr.length - mid];
+
+        System.arraycopy(arr, 0, leftArr, 0, mid);
+        System.arraycopy(arr, mid, rightArr, 0, arr.length - mid);
+
+        mergeSort(leftArr);
+        mergeSort(rightArr);
+
+        merge(arr, leftArr, rightArr);
+    }
+
+    private void merge(int[] array, int[] leftArr, int[] rightArr) {
+        int i = 0, j = 0, k = 0;
+
+        while (i < leftArr.length && j < rightArr.length) {
+            if (leftArr[i] <= rightArr[j]) {
+                array[k++] = leftArr[i++];
+            } else {
+                array[k++] = rightArr[j++];
+            }
+        }
+
+        while (i < leftArr.length) {
+            array[k++] = leftArr[i++];
+        }
+
+        while (j < rightArr.length) {
+            array[k++] = rightArr[j++];
+        }
     }
 
 }
