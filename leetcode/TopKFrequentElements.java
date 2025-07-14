acd26deaafdde0244a05d419174bdaf8e37206bc
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2025-07-14 19:00:46 +0100

    modify TopKFrequentElements

--- leetcode/TopKFrequentElements.java
+++ leetcode/TopKFrequentElements.java
@@ -8,8 +8,36 @@ import java.util.PriorityQueue;
  * EMAIL: damexxey94@gmail.com
  */
 
+/**
+ * Given an integer array nums and an integer k, return the k most frequent elements.
+ * You may return the answer in any order.
+ *
+ * <blockquote>
+ * <pre>
+ * Example 1:
+ * Input: nums = [1,1,1,2,2,3], k = 2
+ * Output: [1,2]
+ *
+ * Example 2:
+ * Input: nums = [1], k = 1
+ * Output: [1]
+ *
+ *
+ * Constraints:
+ * 1 <= nums.length <= 10^5
+ * -10^4 <= nums[i] <= 10^4
+ * k is in the range [1, the number of unique elements in the array].
+ * It is guaranteed that the answer is unique.
+ * F
+ * Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+ * </pre>
+ * </blockquote>
+ */
+
+
 public class TopKFrequentElements {
 
+    // TC -> O(k * log n), SC -> O(k)
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
 
@@ -17,10 +45,11 @@ public class TopKFrequentElements {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
+        PriorityQueue<Integer> maxHeap =
+                new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
 
         for (int key : map.keySet()) {
-            maxHeap.add(key);
+            maxHeap.offer(key);
         }
 
         int[] res = new int[k];
