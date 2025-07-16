ec96414c28bb889a976def07342841c5a87f37ed
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2025-07-16 08:12:00 +0100

    change variable name in ProductOfArrayExceptSelf

--- leetcode/ProductOfArrayExceptSelf.java
+++ leetcode/ProductOfArrayExceptSelf.java
@@ -48,21 +48,21 @@ public class ProductOfArrayExceptSelf {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int[] result = new int[n];
-        int[] left = new int[n];
-        int[] right = new int[n];
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
 
-        left[0] = 1;
+        prefix[0] = 1;
         for (int i = 1; i < n; i++) {
-            left[i] = left[i - 1] * nums[i - 1];
+            prefix[i] = prefix[i - 1] * nums[i - 1];
         }
 
-        right[n - 1] = 1;
+        suffix[n - 1] = 1;
         for (int i = n - 2; i >= 0; i--) {
-            right[i] = right[i + 1] * nums[i + 1];
+            suffix[i] = suffix[i + 1] * nums[i + 1];
         }
 
         for (int k = 0; k < n; k++) {
-            result[k] = left[k] * right[k];
+            result[k] = prefix[k] * suffix[k];
         }
 
         return result;
