96dbb545d452da1c03efdf2ffb19b04c24b88219
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2025-07-16 07:40:26 +0100

    modify ProductOfArrayExceptSelf

--- leetcode/ProductOfArrayExceptSelf.java
+++ leetcode/ProductOfArrayExceptSelf.java
@@ -74,13 +74,16 @@ public class ProductOfArrayExceptSelf {
         int n = nums.length;
         int[] res = new int[n];
 
-        for (int i = 0, temp = 1; i < n; i++) {
-            res[i] = temp;
-            temp *= nums[i];
+        int left = 1;
+        for (int i = 0; i < n; i++) {
+            res[i] = left;
+            left *= nums[i];
         }
-        for (int i = n - 1, temp = 1; i >= 0; i--) {
-            res[i] *= temp;
-            temp *= nums[i];
+
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= right;
+            right *= nums[i];
         }
         return res;
     }
