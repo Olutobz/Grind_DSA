bcd21cf230cc2b01ddc9192113c73ec88f353d91
Onikoyi Damola Olutoba <damexxey94@gmail.com>
2025-08-04 19:10:25 +0100

    add a problem statement to ValidSudoku

--- leetcode/ValidSudoku.java
+++ leetcode/ValidSudoku.java
@@ -1,9 +1,60 @@
 import java.util.HashSet;
 import java.util.Set;
 
+
+/**
+ * Determine if a 9 x 9 Sudoku board is valid.
+ * Only the filled cells need to be validated according to the following rules:
+ * Each row must contain the digits 1-9 without repetition.
+ * Each column must contain the digits 1-9 without repetition.
+ * Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+ * <p>
+ * Note:
+ * A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+ * Only the filled cells need to be validated according to the mentioned rules.
+ *
+ * <blockquote>
+ * <pre>
+ * Example 1:
+ * Input: board =
+ * [["5","3",".",".","7",".",".",".","."]
+ * ,["6",".",".","1","9","5",".",".","."]
+ * ,[".","9","8",".",".",".",".","6","."]
+ * ,["8",".",".",".","6",".",".",".","3"]
+ * ,["4",".",".","8",".","3",".",".","1"]
+ * ,["7",".",".",".","2",".",".",".","6"]
+ * ,[".","6",".",".",".",".","2","8","."]
+ * ,[".",".",".","4","1","9",".",".","5"]
+ * ,[".",".",".",".","8",".",".","7","9"]]
+ * Output: true
+ *
+ * Example 2:
+ * Input: board =
+ * [["8","3",".",".","7",".",".",".","."]
+ * ,["6",".",".","1","9","5",".",".","."]
+ * ,[".","9","8",".",".",".",".","6","."]
+ * ,["8",".",".",".","6",".",".",".","3"]
+ * ,["4",".",".","8",".","3",".",".","1"]
+ * ,["7",".",".",".","2",".",".",".","6"]
+ * ,[".","6",".",".",".",".","2","8","."]
+ * ,[".",".",".","4","1","9",".",".","5"]
+ * ,[".",".",".",".","8",".",".","7","9"]]
+ * Output: false
+ * Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8.
+ * Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+ *
+ *
+ * Constraints:
+ * board.length == 9
+ * board[i].length == 9
+ * board[i][j] is a digit 1-9 or '.'.
+ * </pre>
+ * </blockquote>
+ */
+
 public class ValidSudoku {
 
-    // TC -> O(n^2 + n^2 + n^2) = O(n^2) ~O(9^2), SC -> O(N) = ~O(1) {if we use N = 9}
+    // TC -> O(n^2) ~O(9^2), SC -> O(n) = ~O(1) {if we use n = 9}
     public boolean isValidSudoku(char[][] board) {
         if (board == null || board.length == 0 || board[0].length == 0) {
             return false;
@@ -69,7 +120,7 @@ public class ValidSudoku {
         return true;
     }
 
-    // TC -> O(n^2 + n^2 + n^2) = O(n^2) ~O(9^2), SC -> O(N) = ~O(1) {if we use N = 9}
+    // TC -> O(n^2) ~O(9^2), SC -> O(n) = ~O(1) {if we use n = 9}
     public boolean isValidSudokuII(char[][] board) {
         if (board == null || board.length == 0 || board[0].length == 0) {
             return false;
