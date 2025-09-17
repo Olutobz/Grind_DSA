19a5c948ec6918c9daf0672c1814de7aa6457890
Damola Olutoba Onikoyi <damexxey94@gmail.com>
2025-09-17 12:37:10 +0100

    modify comment for ValidAnagram.java

--- leetcode/java/ValidAnagram.java
+++ leetcode/java/ValidAnagram.java
@@ -18,7 +18,7 @@ import java.util.Map;
  * Output: false
  *
  * Constraints:
- * 1 <= s.length, t.length <= 5 * 104
+ * 1 <= s.length, t.length <= 5 * 10^4
  * s and t consist of lowercase English letters.
  *
  * Follow up: What if the inputs contain Unicode characters?
