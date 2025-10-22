627741fd9a107c77f1c6d16ed0779f6db89dd456
Damola Olutoba Onikoyi <damexxey94@gmail.com>
2025-10-22 08:53:10 +0100

    add problem statement to EncodeAndDecodeStrings.kt

--- leetcode/kotlinVersion/EncodeAndDecodeStrings.kt
+++ leetcode/kotlinVersion/EncodeAndDecodeStrings.kt
@@ -5,5 +5,33 @@ package kotlinVersion
  * DATE: 22, October 2025
  * EMAIL: damexxey94@gmail.com
  */
- 
- 
+
+
+/**
+ * Design an algorithm to encode a list of strings to a single string.
+ * The encoded string is then decoded back to the original list of strings.
+ * Please implement encode and decode
+ *
+ * <blockquote>
+ * <pre>
+ * Example 1:
+ * Input: ["neet","code","love","you"]
+ * Output:["neet","code","love","you"]
+ * Explanation:
+ * One possible encode method is: "lint:;code:;love:;you"
+ *
+ * Example 2:
+ * Input: ["we","say",":","yes"]
+ * Output: ["we","say",":","yes"]
+ *
+ * Constraints:
+ * 0 <= strs.length < 100
+ * 0 <= strs[i].length < 200
+ * strs[i] contains only UTF-8 characters.
+ * </pre>
+ * </blockquote>
+ */
+
+fun encodeAndDecodeStrings() {
+
+}
