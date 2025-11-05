d1d360aaab0e9384b34839e9026cab507e6c7348
Damola Olutoba Onikoyi <damexxey94@gmail.com>
2025-11-05 22:44:21 +0100

    implement DecodeString.kt

--- leetcode/kotlinVersion/DecodeString.kt
+++ leetcode/kotlinVersion/DecodeString.kt
@@ -1,3 +1,41 @@
 package kotlinVersion
 
+import java.util.*
 
+fun decodeString(s: String?): String {
+    if (s.isNullOrEmpty()) return ""
+
+    val intStack = Stack<Int>()
+    val strStack = Stack<StringBuilder>()
+    var res = StringBuilder()
+    var k = 0
+
+    for (ch in s) {
+        when {
+            ch.isDigit() -> {
+                k = k * 10 + (ch - '0')
+            }
+
+            ch == '[' -> {
+                intStack.push(k)
+                strStack.push(res)
+                res = StringBuilder()
+                k = 0
+            }
+
+            ch == ']' -> {
+                val temp = res
+                res = strStack.pop()
+                repeat(intStack.pop()) {
+                    res.append(temp)
+                }
+            }
+
+            else -> {
+                res.append(ch)
+            }
+        }
+    }
+
+    return res.toString()
+}
